e45aa1669f5f62063f8fb3182fe33342b14b69cd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-16 15:09:55 -0400

    Update Part4.sh

--- Part4.sh
+++ Part4.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 { whiptail --msgbox "During the install, multiple webservers were installed. Let's try to adjust the configs to not interfere with eachother" 20 70 1
 source /home/dbbvariables
 LIGHT_BIND=`sed -n '/server.port                 = 80/=' /etc/lighttpd/lighttpd.conf`
@@ -23,10 +22,10 @@ sudo sed -i "s/80/$NEW_APACHE80/" /etc/apache2/ports.conf
 sudo sed -i "s/80/$NEW_APACHE80/" /etc/apache2/sites-enabled/000-default.conf
 sudo sed -i "s/443/$NEW_APACHE443/" /etc/apache2/ports.conf
 sudo sed -i "s/443/$NEW_APACHE443/" /etc/apache2/sites-enabled/000-default.conf
-sudo sed -i "s/80/$NEW_NGINX80/" /etc/nginx/sites-available/default
-sudo sed -i '$LIGHT_BIND a\server.bind                 = "$NEW_LIGHTTPDBIND"' /etc/lighttpd/lighttpd.conf
+sudo sed -i "s/listen 80 default_server/listen $NEW_NGINX80 default_server/" /etc/nginx/sites-available/default
+sudo sed -i "$LIGHT_BIND a\server.bind                 = "$NEW_LIGHTTPDBIND"' /etc/lighttpd/lighttpd.conf
 sudo sed -i "s/80/$NEW_LIGHTTPD80/" /etc/lighttpd/lighttpd.conf
 sudo service nginx restart
 sudo service apache2 restart
 sudo service lighttpd restart
-fi }
+fi  }
