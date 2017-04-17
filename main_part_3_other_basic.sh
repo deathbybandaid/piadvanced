f12bb50867046451b73a45ae6f0d870838859039
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-17 09:48:50 -0400

    Create main_part_3_other_basic.sh

---
+++ main_part_3_other_basic.sh
@@ -0,0 +1,23 @@
+{
+  dpkg-reconfigure tzdata
+}
+
+{ whiptail --yesno "Would you like the SSH server enabled or disabled?" 20 60 2 \
+--yes-button Enable --no-button Disable
+RET=$?
+if [ $RET -eq 0 ]; then
+update-rc.d ssh enable &&
+invoke-rc.d ssh start &&
+whiptail --msgbox "SSH server enabled" 20 60 1
+elif [ $RET -eq 1 ]; then
+update-rc.d ssh disable &&
+whiptail --msgbox "SSH server disabled" 20 60 1
+else
+return $RET
+fi }
+
+{ if (whiptail --yesno "Do you plan on running headless? This will set the memory split to 16." 8 78) then
+sudo sed -i "s/\($gpu_mem *= *\).*/\1$116/"  /boot/config.txt
+else
+echo ""
+fi }
