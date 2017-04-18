be0be17ff3b21e363cbf82b4d7b5f49ee141d63b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 19:28:53 -0400

    Update pihole.sh

--- pihole.sh
+++ pihole.sh
@@ -106,10 +106,12 @@ echo ""
 fi }
 
 ## Experimental youtube ad blocking
-{ if (whiptail --yesno "Do you want to use an script to add additional youtube adblocking?" 8 78) then
-sudo wget https://raw.githubusercontent.com/deathbybandaid/youtubeadblock/master/youtubeadblock.sh -P /etc/piadvanced/installscripts/
+{ if (whiptail --yesno "Do you want to use an script to add additional adblocking?" 8 78) then
+cd /etc/piadvanced/installscripts/
+sudo wget https://raw.githubusercontent.com/deathbybandaid/youtubeadblock/master/youtubeadblock.sh
 (crontab -l ; echo "0 2 * * * /etc/piadvanced/installscripts/youtubeadblock/youtube-ads.sh") | crontab -
 sudo echo "http://localhost/admin/youtube.txt" | sudo tee --append /etc/pihole/adlists.list
 else
 echo ""
 fi }
+
