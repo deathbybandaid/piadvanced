77687da2d8d79ca2d7ab6190d1214c382c439b7b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:21:39 -0400

    Update usermin.sh

--- usermin.sh
+++ usermin.sh
@@ -1,5 +1,4 @@
 ##Usermin
-## 
 { if (whiptail --yesno "Do you want to install Usermin?" 8 78) then
 sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl
 sudo wget http://prdownloads.sourceforge.net/webadmin/usermin_1.701_all.deb
