87934acb4a9427bd63b52b7bfa6dbf15ca078643
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:18:53 -0400

    Update memorysplit.sh

--- memorysplit.sh
+++ memorysplit.sh
@@ -2,6 +2,8 @@
 { if (whiptail --yesno "Let's set the memory split." 8 78) then
 { NEWMEM_SPLIT=$(whiptail --inputbox "Do you plan on running headless? If so, set the memory split to 16." 20 60 "16" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
+sudo mkdir /etc/piadvanced/backups/boot
+sudo cp /boot/config.txt /etc/piadvanced/backups/boot/
 sudo sed -i '/gpu_mem/ d' /boot/config.txt
 sudo echo "gpu_mem=$NEWMEM_SPLIT" | sudo tee --append /boot/config.txt
 fi }
