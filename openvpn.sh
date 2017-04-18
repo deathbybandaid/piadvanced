d64baa358e6114db9eca39f6788d39edaaf7bebc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 15:20:11 -0400

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ## OpenVPN
 { if (whiptail --yesno "Do you plan on using the OpenVPN Server?" 8 78) then
-sudo wget https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -P /home/installs/
-sudo chmod +x /home/installs/openvpn-install.sh
-sudo bash /home/installs/openvpn-install.sh
+sudo wget https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -P /etc/piadvanced/installscripts/
+sudo chmod +x /etc/piadvanced/installscripts/openvpn-install.sh
+sudo bash /etc/piadvanced/installscripts/openvpn-install.sh
 else
 echo ""
 fi }
