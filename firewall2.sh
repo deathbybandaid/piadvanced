31b2670f1152bda3b4b83aad34fbb985bc82f9b3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 16:43:20 -0400

    Update firewall2.sh

--- firewall2.sh
+++ firewall2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+source /etc/piadvanced/install/variables.conf
 ## IPTables Firewall
 ## This will create a script to make sure the firewall is intact at boot , and every 6 hours.
 { if (whiptail --yesno "Do you want Activate a firwall created based on this install?" 8 78) then
@@ -11,6 +12,23 @@ else
 echo ""
 fi }
 
-#sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "#  Drop all other inbound - default deny unless explicitly allowed policy" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -j DROP" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A FORWARD -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
 
+sudo echo "COMMIT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
 
+sudo echo "*nat" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo ":PREROUTING ACCEPT [264:40146]" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo ":INPUT ACCEPT [48:3296]" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo ":OUTPUT ACCEPT [192:14170]" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo ":POSTROUTING ACCEPT [192:14170]" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+
+sudo echo "Openvpn (If Selected)" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "$OPENVPN_NAT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+
+sudo echo "COMMIT" | sudo tee --append /etc/iptables.firewall.rules
