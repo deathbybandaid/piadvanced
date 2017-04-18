c5a5c463771e59bbd2b3fa4a4c8e616fa0f25604
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:14:06 -0400

    Create sources.sh

---
+++ sources.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+## Sources
+{ whiptail --msgbox "I'm going to add sources ." 20 70 1 
+sudo mkdir /etc/piadvanced/backups/sources
+sudo cp /etc/apt/sources.list /etc/piadvanced/backups/sources/
+sudo cp /etc/apt/apt.conf.d/99-default-release /etc/piadvanced/backups/sources/
+sudo cp /etc/apt/sources.list.d/stretch.list /etc/piadvanced/backups/sources/
+sudo echo 'deb http://repozytorium.mati75.eu/raspbian jessie-backports main contrib non-free' | sudo tee --append /etc/apt/sources.list
+sudo echo 'deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi' | sudo tee --append /etc/apt/sources.list.d/stretch.list
+sudo echo 'APT::Default-Release "jessie";' | sudo tee --append /etc/apt/apt.conf.d/99-default-release
+sudo gpg --keyserver pgpkeys.mit.edu --recv-key CCD91D6111A06851
+sudo gpg --armor --export CCD91D6111A06851 | apt-key add -
+sudo wget https://archive.raspbian.org/raspbian.public.key -O - | sudo apt-key add -
+ }
