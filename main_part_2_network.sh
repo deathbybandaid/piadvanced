5094107422a8fc9651767bdf26a0194596e7a02f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 11:23:13 -0400

    Update main_part_2_network.sh

--- main_part_2_network.sh
+++ main_part_2_network.sh
@@ -3,7 +3,7 @@
 OLD_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
 NEW_HOSTNAME=$(whiptail --inputbox "Please enter a hostname" 20 60 "$OLD_HOSTNAME" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
-sudo echo "NEW_HOSTNAME=$NEW_HOSTNAME" | sudo tee --append /home/dbbvariables
+sudo echo "NEW_HOSTNAME=$NEW_HOSTNAME" | sudo tee --append /etc/piadvanced/install/variables.conf
 sudo echo "$NEW_HOSTNAME" > /etc/hostname
 sudo sed -i "s/127.0.1.1.*$OLD_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
 fi }
@@ -14,7 +14,7 @@ OLDETH_IP=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
 OLDETH_GATEWAY=`ip route show 0.0.0.0/0 dev eth0 | cut -d\  -f3`
 NEWETH_IP=$(whiptail --inputbox "Please enter desired IP for eth0" 20 60 "$OLDETH_IP" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
-sudo echo "NEWETH_IP=$NEWETH_IP" | sudo tee --append /home/dbbvariables
+sudo echo "NEWETH_IP=$NEWETH_IP" | sudo tee --append /etc/piadvanced/install/variables.conf
 sudo echo "" | sudo tee --append /etc/dhcpcd.conf
 sudo echo "#eth0" | sudo tee --append /etc/dhcpcd.conf
 sudo echo "interface eth0" | sudo tee --append /etc/dhcpcd.conf
@@ -32,9 +32,9 @@ whiptail --msgbox "Let's set a static IP using wlan0" 20 70 1
 OLDWLAN_GATEWAY=`ip route show 0.0.0.0/0 dev wlan0 | cut -d\  -f3`
 NEWWLAN_IP=$(whiptail --inputbox "Please enter desired IP for wlan0" 20 60 "$OLDWLAN_IP" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
-sudo echo "NEWWLAN_IP=$NEWWLAN_IP" | sudo tee --append /home/dbbvariables
-sudo echo "NEW_SSID=$NEW_SSID" | sudo tee --append /home/dbbvariables
-sudo echo "NEW_PSK=$NEW_PSK" | sudo tee --append /home/dbbvariables
+sudo echo "NEWWLAN_IP=$NEWWLAN_IP" | sudo tee --append /etc/piadvanced/install/variables.conf
+sudo echo "NEW_SSID=$NEW_SSID" | sudo tee --append /etc/piadvanced/install/variables.conf
+sudo echo "NEW_PSK=$NEW_PSK" | sudo tee --append /etc/piadvanced/install/variables.conf
 sudo echo "" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
 sudo echo "## Wifi" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
 sudo echo "network={"| sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
