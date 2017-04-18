5cdf3530c306eea8b05c16766e8b30e121c0d57c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:21:01 -0400

    Update randomnumberfix.sh

--- randomnumberfix.sh
+++ randomnumberfix.sh
@@ -1,6 +1,7 @@
 ## 
 { if (whiptail --yesno "Do you wnat to install the random number fix?" 8 78) then
 sudo apt-get install -y rng-tools
+sudo cp /etc/default/rng-tools /etc/piadvanced/backups/
 sudo echo 'HRNGDEVICE=/dev/urandom' | sudo tee --append /etc/default/rng-tools
 else
 echo ""
