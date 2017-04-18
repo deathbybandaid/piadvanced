a7c34a8cea78a10407607bb2bb71cf6546778f94
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-17 20:12:31 -0400

    Update memorysplit.sh

--- memorysplit.sh
+++ memorysplit.sh
@@ -10,27 +10,14 @@ echo ""
 fi }
 
 ## This is an experimental tweak to unlock the pi's missing 16MB
-whiptail --msgbox "This is The Deathbybandaid Pihole Install" 20 70 1
 { if (whiptail --yesno "Do you want to try the experimental unlock of an extra 16MB. This is for the Pi2 and Pi3 only" 8 78) then
-
-else
-echo ""
-fi }
-
-total_mem=1024
-
-
-{ NEWMEM_SPLIT=$(whiptail --inputbox "Do you plan on running headless? If so, set the memory split to 16." 20 60 "16" 3>&1 1>&2 2>&3)
+{ NEWMEM_UNLOCK=$(whiptail --inputbox "Setting to 1024 is the Maximum. It may be prudent to say 1024" 20 60 "16" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
-sudo sed -i "s/\($gpu_mem *= *\).*/\1$116/"  /boot/config.txt
+sudo sed -i '/gpu_mem/ d' /boot/config.txt
+sudo echo "gpu_mem=$NEWMEM_SPLIT" | sudo tee --append /boot/config.txt
 fi }
-
-
-
-
-## This sets the memory split down to 16
-#{ if (whiptail --yesno "Do you plan on running headless? This will set the memory split to 16." 8 78) then
-#sudo sed -i "s/\($gpu_mem *= *\).*/\1$116/"  /boot/config.txt
-#else
-#echo ""
-#fi }
+sudo sed -i '/total_mem/ d' /boot/config.txt
+sudo echo "gpu_mem=$NEWMEM_UNLOCK" | sudo tee --append /boot/config.txt
+else
+sudo sed -i '/total_mem/ d' /boot/config.txt
+fi }
