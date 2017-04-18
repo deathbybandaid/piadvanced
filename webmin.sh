bf7b3c44964ce90d72d315cfe92847edf3a891bc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:21:55 -0400

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,5 +1,4 @@
 ##Webmin
-## 
 { if (whiptail --yesno "Do you want to install Webmin?" 8 78) then
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
