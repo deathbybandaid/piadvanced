17ac1091a610ebd94e9a6275632919a0ec5bcd48
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 16:18:54 -0400

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -2,6 +2,13 @@
 ## XRDP
 { if (whiptail --yesno "Do you want to install xrdp?" 8 78) then
 sudo apt-get install -y xrdp
+sudo echo "# XRDP" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -p tcp --dport 3389 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -p udp --dport 3389 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -p tcp --dport 3350 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -p udp --dport 3350 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -p tcp --dport 5910 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -p udp --dport 5910 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
 else
 echo ""
 fi }
