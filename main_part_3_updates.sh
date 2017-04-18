a021d6c60f72ea01faac4979ae7c535a6082426f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-17 20:51:08 -0400

    Update main_part_3_updates.sh

--- main_part_3_updates.sh
+++ main_part_3_updates.sh
@@ -17,22 +17,11 @@ sudo apt-get clean
  }
 { whiptail --msgbox "I'm going to install some stuff." 20 70 1
 sudo apt-get install raspi-config -y
-sudo apt-get install -t stretch -y nginx-full
-sudo apt-get install -t stretch -y php7.0
-sudo apt-get install -t stretch -y php7.0-curl
-sudo apt-get install -t stretch -y php7.0-gd
-sudo apt-get install -t stretch -y php7.0-fpm
-sudo apt-get install -t stretch -y php7.0-opcache
-sudo apt-get install -t stretch -y php7.0-opcache
-sudo apt-get install -t stretch -y php7.0-mbstring
-sudo apt-get install -t stretch -y php7.0-xml 
-sudo apt-get install -t stretch -y php7.0-zip
 sudo apt-get install -y zip
 sudo apt-get install -y unzip
 sudo apt-get install -y build-essential
 sudo apt-get install -y wget
 sudo apt-get install -y checkinstall
-sudo apt-get install -y fail2ban
 sudo apt-get install -y git
 sudo apt-get install -y install perl
 sudo apt-get install -y libnet-ssleay-perl
@@ -44,9 +33,15 @@ sudo apt-get install -y ty-perl
 sudo apt-get install -y apt-show-versions
 sudo apt-get install -y python
 sudo apt-get install -y dnsutils
-sudo apt-get install -y rng-tools
-sudo echo 'HRNGDEVICE=/dev/urandom' | sudo tee --append /etc/default/rng-tools
-sudo apt-get install -y xrdp
+sudo apt-get install -t stretch -y php7.0
+sudo apt-get install -t stretch -y php7.0-curl
+sudo apt-get install -t stretch -y php7.0-gd
+sudo apt-get install -t stretch -y php7.0-fpm
+sudo apt-get install -t stretch -y php7.0-opcache
+sudo apt-get install -t stretch -y php7.0-opcache
+sudo apt-get install -t stretch -y php7.0-mbstring
+sudo apt-get install -t stretch -y php7.0-xml 
+sudo apt-get install -t stretch -y php7.0-zip
 sudo apt-get install -y python-pip
 sudo apt-get install -y apt-utils
 sudo apt-get install -y debconf
@@ -61,5 +56,4 @@ sudo apt-get install -y iputils-ping
 sudo apt-get install -y lsof
 sudo apt-get install -y netcat
 sudo apt-get install -y sudo
-sudo apt-get install -y lighttpd
  }
