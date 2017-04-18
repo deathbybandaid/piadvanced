9c1ccf5b71702056b4e3ea4c77ae9ad29ac43a52
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:16:29 -0400

    Update main_part_4_web.sh

--- main_part_4_web.sh
+++ main_part_4_web.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-whiptail --msgbox "During the install, multiple webservers could be installed. Let's try to adjust the configs to not interfere with eachother" 20 70 1
+whiptail --msgbox "During the install, multiple webservers could have been installed. Let's try to adjust the configs to not interfere with eachother" 20 70 1
 whiptail --msgbox "If these aren't set correctly, you will have to manually adjust them, because they may not start properly." 20 70 1
 
 { if (whiptail --yesno "Do you Want to install Lighttpd?" 8 78) then
