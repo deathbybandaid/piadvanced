450d4bccd601ca9cab5c2a34eeca3f70d998dd44
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:27:45 -0400

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -1,6 +1,7 @@
-## POSSIBLE INSTALL OF DNSCRYPT
+#!/bin/sh
+## DNSMasq
 { if (whiptail --yesno "Do you plan on using dnsmasq?" 8 78) then
-{ if (whiptail --yesno "Do you want to use the experimenta version 2.77test4?" 8 78) then
+{ if (whiptail --yesno "Do you want to use the experimental version 2.77test4?" 8 78) then
 sudo wget https://raw.githubusercontent.com/deathbybandaid/dnsmasqUpdate/master/dnsmasqupgrade.sh -P /etc/piadvanced/installscripts/
 sudo bash /etc/piadvanced/installscripts/dnsmasqupgrade.sh
 else
