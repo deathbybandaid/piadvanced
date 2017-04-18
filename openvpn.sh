5bd360e5117891bd72c77bf6177b6bbc38e75778
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 16:43:13 -0400

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -5,6 +5,9 @@ source /etc/piadvanced/install/variables.conf
 sudo wget https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -P /etc/piadvanced/installscripts/
 sudo chmod +x /etc/piadvanced/installscripts/openvpn-install.sh
 sudo bash /etc/piadvanced/installscripts/openvpn-install.sh
+sudo echo "# VPN" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "-A INPUT -p tcp --dport 1194 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "OPENVPN_NAT=-A POSTROUTING -s 10.8.0.0/24 -j SNAT --to-source $NEWETH_IP" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
 echo ""
