d99742bbc3b2c3c88c33d981684e968dbd90b1fb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 14:51:41 -0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -15,3 +15,14 @@ fi  }
 else
 echo ""
 fi }
+
+## openssl 
+sudo openssl dhparam -out /etc/nginxdh2048.pem 2048
+
+## Perfect forward secrecy
+sudo echo "ssl_ciphers "ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS";" | sudo tee --append /etc/nginxperfect-forward-secrecy.conf
+sudo echo "ssl_dhparam /etc/nginx/dh2048.pem;" | sudo tee --append /etc/nginxperfect-forward-secrecy.conf
+sudo echo "add_header Strict-Transport-Security "max-age=31536000; includeSubdomains;";" | sudo tee --append /etc/nginxperfect-forward-secrecy.conf
+
+## Let's Encrypt
+sudo bash /etc/piadvanced/letsencrypt.sh
