42489bc8762f1be7cb0fe1996c83e4c25c759988
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 16:37:17 -0400

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+source /etc/piadvanced/install/variables.conf
 ## OpenVPN
 { if (whiptail --yesno "Do you plan on using the OpenVPN Server?" 8 78) then
 sudo wget https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -P /etc/piadvanced/installscripts/
 sudo chmod +x /etc/piadvanced/installscripts/openvpn-install.sh
 sudo bash /etc/piadvanced/installscripts/openvpn-install.sh
+sudo echo "OPENVPN_NAT=-A POSTROUTING -s 10.8.0.0/24 -j SNAT --to-source $NEWETH_IP" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
 echo ""
 fi }
