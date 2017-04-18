e87ccf294c81f7a0677862767909aec79627dc73
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 15:16:29 -0400

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -3,5 +3,5 @@
 #sudo git clone https://github.com/certbot/certbot /etc/letsencrypt
 #sudo /etc/letsencrypt/certbot-auto certonly --agree-tos --webroot -w /data/mysite.com/www -d mysite.com -d www.mysite.com -d mail.mysite.com -d srv01.mysite.com
 
-# 0 6 * * * /etc/letsencrypt/certbot/certbot-auto renew --text >> /etc/letsencrypt/certbot/certbot-cron.log && sudo service nginx reload
+#(crontab -l ; echo "0 6 * * * /etc/letsencrypt/certbot/certbot-auto renew --text >> /etc/letsencrypt/certbot/certbot-cron.log && sudo service nginx reload") | crontab -
 
