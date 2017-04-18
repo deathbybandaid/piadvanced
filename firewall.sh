775de08ce8859ca1ae21d4d43c7c72a96f3694ae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 15:43:39 -0400

    Create firewall.sh

---
+++ firewall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# sudo nano /etc/iptables.firewall.rules
+#
+# sudo iptables-restore < /etc/iptables.firewall.rules
+#
+# sudo nano /etc/network/if-pre-up.d/firewall
+#
+# #!/bin/sh
+# /sbin/iptables-restore < /etc/iptables.firewall.rules
+#
+# sudo chmod +x /etc/network/if-pre-up.d/firewall
+#
+# sudo cp /etc/network/if-pre-up.d/firewall /etc/piadvanced/installscripts/firewall.sh
+# 
+# (crontab -l ; echo "0 */6 * * * sudo bash /etc/piadvanced/installscripts/firewall.sh") | crontab -
