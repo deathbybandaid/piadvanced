dfd9b2f34d061665e5e39f18e746d001899ddcd7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 15:56:43 -0400

    Update firewall2.sh

--- firewall2.sh
+++ firewall2.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 ## IPTables Firewall
-
 ## This will create a script to make sure the firewall is intact at boot , and every 6 hours.
+{ if (whiptail --yesno "Do you want Activate a firwall created based on this install?" 8 78) then
 sudo echo "#!/bin/sh" | sudo tee --append /etc/network/if-pre-up.d/firewall
 sudo echo "/sbin/iptables-restore < /etc/iptables.firewall.rules" | sudo tee --append /etc/network/if-pre-up.d/firewall
 sudo chmod +x /etc/network/if-pre-up.d/firewall
 sudo cp /etc/network/if-pre-up.d/firewall /etc/piadvanced/installscripts/firewall.sh 
 (crontab -l ; echo "0 */6 * * * sudo bash /etc/piadvanced/installscripts/firewall.sh") | crontab -
+else
+echo ""
+fi }
 
 #sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
 
