df3d894c35b618ad49910ca7be5840ea5622d36a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 13:55:11 -0400

    Update pihole.sh

--- pihole.sh
+++ pihole.sh
@@ -20,6 +20,32 @@ else
 echo ""
 fi }
 
+## Dark Theme
+{ if (whiptail --yesno "Do you want to install Dark webui theme?" 8 78) then
+sudo wget https://raw.githubusercontent.com/lkd70/PiHole-Dark/master/install.sh -P /var/www/html/
+cd /var/www/html
+chmod +x install.sh
+sudo bash install.sh
+else
+echo ""
+fi }
+
+## Wall3k Block Page
+{ if (whiptail --yesno "Do you want to install the wally3k block page?" 8 78) then
+[ -f "/var/phbp.ini" ] && sudo mv /var/phbp.ini /var/phbp.ini.BAK
+html=$(grep server.document-root /etc/lighttpd/lighttpd.conf | awk -F\" '{print $2}')
+sudo wget -q https://raw.githubusercontent.com/WaLLy3K/Pi-hole-Block-Page/master/index.php -O "$html/index.php"
+sudo wget -q https://raw.githubusercontent.com/WaLLy3K/Pi-hole-Block-Page/master/phbp.ini -O "/var/phbp.ini"
+sudo chmod 755 "$html/index.php"
+[ -f "/var/phbp.php" ] && sudo mv /var/phbp.php /var/phbp.old.BAK
+[ ! -d "/etc/lighttpd/conf-enabled" ] && sudo mkdir -m 755 /etc/lighttpd/conf-enabled
+[ ! -f "/etc/lighttpd/conf-enabled/phbp.conf" ] && echo -e '# Pi-hole "server.error-handler-404" override\nurl.rewrite-once = ( "pihole/index.php" => "/index.php" )' | sudo tee /etc/lighttpd/conf-enabled/phbp.conf
+echo "Done! Please edit '/var/phbp.ini' to customise your install"
+sudo service lighttpd force-reload
+else
+echo ""
+fi }
+
 ## Bypass
 { if (whiptail --yesno "Do you want to install a dnsmasq tweak to bypass pihole by mac address?" 8 78) then
 sudo wget https://raw.githubusercontent.com/deathbybandaid/pihole-bypass/master/04-bypass.conf -P /etc/dnsmasq.d/
