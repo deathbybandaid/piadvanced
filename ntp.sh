827493761251057d08388632b6d862edc3b361b1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 12:20:08 -0400

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -1,6 +1,7 @@
 ## NTP
 { if (whiptail --yesno "Do you want to set ntp servers? serverlist is available at http://support.ntp.org/bin/view/Servers/NTPPoolServers" 8 78) then
 NEW_NTP=$(whiptail --inputbox "Set server here" 20 60 "debian.pool.ntp.org" 3>&1 1>&2 2>&3)
+sudo cp /etc/ntp.conf /etc/piadvanced/backups/
 sudo sed -i "s/pool 0.debian.pool.ntp.org iburst/pool 0.$NEW_NTP iburst/" /etc/ntp.conf
 sudo sed -i "s/pool 1.debian.pool.ntp.org iburst/pool 1.$NEW_NTP iburst/" /etc/ntp.conf
 sudo sed -i "s/pool 2.debian.pool.ntp.org iburst/pool 2.$NEW_NTP iburst/" /etc/ntp.conf
@@ -12,10 +13,10 @@ else
 echo ""
 fi }
 
-{ if (whiptail --yesno "Do you want to set a script to refresh time?" 8 78) then
+#{ if (whiptail --yesno "Do you want to set a script to refresh time?" 8 78) then
 
 #cronjob sudo service ntp stop && sudo ntpd -gq && sudo service ntp start
 
-else
-echo ""
-fi }
+#else
+#echo ""
+#fi }
