eba1c6f3cdc253c37c09a6f8aa8765ea3bf51bb6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 18:26:39 -0400

    Update rpimonitor.sh

--- rpimonitor.sh
+++ rpimonitor.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 #rpimonitor
-
+{ if (whiptail --yesno "Do you want to install rpimonitor?" 8 78) then
+sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
+sudo apt-get install -y rpimonitor
+sudo /etc/init.d/rpimonitor update
 sudo echo "# rpi monitor" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "-A INPUT -p tcp --dport 8889 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+else
+echo ""
+fi }
