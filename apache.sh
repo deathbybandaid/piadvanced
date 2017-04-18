9a3cb14ff2f8206eda5c1572365bd42090c52355
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-17 20:50:34 -0400

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -7,6 +7,7 @@ whiptail --msgbox "I suggest changing Apache Ports to something like 85 and 445"
 NEW_APACHE80=$(whiptail --inputbox "Change the default port 80 for Apache" 20 60 "85" 3>&1 1>&2 2>&3)
 NEW_APACHE443=$(whiptail --inputbox "Change the default port 443 for Apache" 20 60 "445" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
+sudo apt-get install -y apache2
 sudo cp -r /etc/apache2/ /etc/piadvanced/backups/apache2/
 sudo echo "NEW_APACHE80=$NEW_APACHE80" | sudo tee --append /home/dbbvariables
 sudo echo "NEW_APACHE443=$NEW_APACHE443" | sudo tee --append /home/dbbvariables
