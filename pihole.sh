c570fb09536c0b59ab8e6f2d623c794d0790c850
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 13:46:21 -0400

    Update pihole.sh

--- pihole.sh
+++ pihole.sh
@@ -13,9 +13,63 @@ fi }
   if [ $? -eq 0 ]; then pihole -a -p $NEW_PASS
 fi }
 
+## Wally3k Adlists
+{ if (whiptail --yesno "Do you want to install the Wally3k adlists.list?" 8 78) then
+sudo wget https://raw.githubusercontent.com/deathbybandaid/wally3k-adlists.list/master/adlists.list -P /etc/pihole/
+else
+echo ""
+fi }
 
-{ whiptail --msgbox "Installing some dnsmasq tweaks. See Readme" 20 70 1
+## Bypass
+{ if (whiptail --yesno "Do you want to install a dnsmasq tweak to bypass pihole by mac address?" 8 78) then
 sudo wget https://raw.githubusercontent.com/deathbybandaid/pihole-bypass/master/04-bypass.conf -P /etc/dnsmasq.d/
+else
+echo ""
+fi }
+
+## Add Interfaces
+{ if (whiptail --yesno "Do you want to install a dnsmasq tweak to add interfaces for pihole to listen on?" 8 78) then
 sudo wget https://raw.githubusercontent.com/deathbybandaid/piholeinterfaces/master/05-addint.conf -P /etc/dnsmasq.d/
+else
+echo ""
+fi }
+
+## Active Directory
+{ if (whiptail --yesno "Do you want to install a dnsmasq tweak to add your active directory dns servers to pihole?" 8 78) then
 sudo wget https://raw.githubusercontent.com/deathbybandaid/piholeAD/master/06-activedirectory.conf -P /etc/dnsmasq.d/
- }
+else
+echo ""
+fi }
+
+## pihole -up 30 minutes
+{ if (whiptail --yesno "Do youw want to run pihole -up every 30 minutes?" 8 78) then
+sudo wget https://raw.githubusercontent.com/deathbybandaid/piholeautoupdate/master/piholeautoupdate.sh -P /etc/piadvanced/installscripts/
+# 0,30 * * * * bash /etc/piadvanced/installscripts/piholeautoupdate.sh
+else
+echo ""
+fi }
+
+## Weekly old list removal
+{ if (whiptail --yesno "Do you want a script to keep cached lists from becoming stale?" 8 78) then
+sudo wget https://raw.githubusercontent.com/deathbybandaid/piholefreshlists/master/piholefreshlists.sh -P /etc/piadvanced/installscripts/
+# 0 5 * * 1 /etc/piholescripts/piholefreshlists.sh
+else
+echo ""
+fi }
+
+## Gravity 6 hours
+{ if (whiptail --yesno "Do you want a script to update gravity every 6 hours?" 8 78) then
+sudo wget https://raw.githubusercontent.com/deathbybandaid/piholegravity/master/piholegravity.sh -P /etc/piadvanced/installscripts/
+# 0 */6 * * * sudo bash /etc/piholescripts/piholegravity.sh
+else
+echo ""
+fi }
+
+## 
+{ if (whiptail --yesno "Do you want a program to parse additional lists? By default this uses lists used by ublock. To change the lists, edit lists.lst in /etc/piadvanced/installscripts/ublockpihole/" 8 78) then
+git clone https://github.com/deathbybandaid/ublockpihole.git /etc/piadvanced/installscripts/ublockpihole/
+# 0 1 * * * /etc/piholescripts/ublockpihole/ublockpihole.sh
+# append adlists.list
+else
+echo ""
+fi }
