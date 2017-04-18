e99e6b16783564572103e1c8c52a363178c440f4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 11:03:22 -0400

    Create psad.sh

---
+++ psad.sh
@@ -0,0 +1,10 @@
+## 
+{ if (whiptail --yesno "Do you want to install psad?" 8 78) then
+echo "Stay tuned"
+sudo wget http://cipherdyne.org/psad/download/psad-2.4.4.tar.gz -P /etc/piadvanced/installscripts/
+sudo tar xzf psad-2.4.4.tar.gz
+cd psad-2.4.4
+sudo ./install.pl
+else
+echo ""
+fi }
