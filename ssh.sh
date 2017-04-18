4d5faf009a77c6f861f8638f96c54108df25e0b9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-18 16:27:17 -0400

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -14,9 +14,6 @@ sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
 elif [ $RET -eq 1 ]; then
 update-rc.d ssh disable &&
 whiptail --msgbox "SSH server disabled" 20 60 1
-sudo echo "# SSH" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "-A INPUT -p tcp -m state --state NEW --dport 22 -j DROP" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
 else
 return $RET
 fi }
