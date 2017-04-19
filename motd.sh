75dc9a741625db84f465de01270cc3632e652bfe
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-19 08:47:36 -0400

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -3,8 +3,8 @@
 { if (whiptail --yesno "Do you want to Change your message of the day to a much cooler one?" 8 78) then
 whiptail --msgbox "This is the message you receive at login" 20 70 1
 sudo systemctl disable motd
-mkdir /etc/update-motd.d
-rm -f /etc/motd
+sudo mkdir /etc/update-motd.d
+sudo rm -f /etc/motd
 sudo wget https://raw.githubusercontent.com/deathbybandaid/pimotd/master/10logo -P /etc/update-motd.d/
 else
 echo ""
