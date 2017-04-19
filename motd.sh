70acf5137a8972aeff22f534cd7b337c5acabdf6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-19 08:51:12 -0400

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -2,6 +2,7 @@
 ## Message of the day
 { if (whiptail --yesno "Do you want to Change your message of the day to a much cooler one?" 8 78) then
 whiptail --msgbox "This is the message you receive at login" 20 70 1
+sudo uname -snrvm > /var/run/motd.dynamic
 sudo systemctl disable motd
 sudo mkdir /etc/update-motd.d
 sudo rm -f /etc/motd
