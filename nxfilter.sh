534f08fa8da811244fe132969704050a5e243432
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 11:27:45 -0400

    Update nxfilter.sh

--- nxfilter.sh
+++ nxfilter.sh
@@ -1,27 +1,15 @@
-## https://qrys.ch/running-nxfilter-on-a-headless-raspberry-pi/
+#!/bin/sh
+## nxfilter
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install nxfilter?" 8 78) then
 
 
-## the stuff below is for dnsmasq
 
-# From here entries from Jeroen
-local=/noads.local/
-domain=noads.local
-address=/raspberry.noads.local/192.168.1.2
-address=/wpad.noads.local/192.168.1.2
-address=/wpad/192.168.1.2
-dhcp-authoritative
-#dhcp-range=set:red,192.168.1.100,192.168.1.199,255.255.255.0,72h
-dhcp-range=set:red,192.168.1.100,192.168.1.199,255.255.255.0,365d
-dhcp-option=tag:red,6,192.168.1.4,192.168.1.2,208.67.222.222,8.8.8.8
-dhcp-option=tag:red,option:router,192.168.1.1
-# dhcp-option=tag:red,option:ntp-server,192.168.1.2
-dhcp-option=19,0
-dhcp-option=42,0.0.0.0
-dhcp-option=44,192.168.1.2
-dhcp-option=45,0.0.0.0
-dhcp-option=46,8
-all-servers
-txt-record=wpad,"service:wpad:!http://192.168.1.2:80/wpad.dat?"
-srv-host=wpad.tcp.wpad,wpad.wpad,80
-srv-host=_wpad._tcp.192.168.1.2,wpad.192.168.1.2,80
-dhcp-option=252,"http://192.168.1.2/wpad.dat?"
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
