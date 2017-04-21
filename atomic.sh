072a15166785e5644b64cf24f3d2a4259fb31ef0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 10:29:05 -0400

    Update atomic.sh

--- atomic.sh
+++ atomic.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ## AtoMiC-ToolKit
-{ if (whiptail --yesno "Do you want to install and use the AtoMiC-ToolKit for htpc softwares?" 8 78) then
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the AtoMiC-ToolKit for htpc softwares?" 8 78) then
+echo "User Declined Atomic"
+else
 sudo git clone https://github.com/htpcBeginner/AtoMiC-ToolKit ~/AtoMiC-ToolKit
 cd ~/AtoMiC-ToolKit/setup.sh
-else
-echo ""
 fi }
