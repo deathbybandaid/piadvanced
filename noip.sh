361e9f95a4f052148368b9dd61970a342e932e95
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 11:23:48 -0400

    Update noip.sh

--- noip.sh
+++ noip.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ## No-IP
-{ if (whiptail --yesno "Do you plan on using the No-IP dynamic Update Client?" 8 78) then
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using the No-IP dynamic Update Client?" 8 78) then
+echo "User Declined NOIP DUC"
+else
 sudo mkdir /home/installs/noip
 sudo wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz -P /etc/piadvanced/installscripts/noip/
 cd /etc/piadvanced/installscripts/noip/
@@ -10,6 +12,4 @@ sudo make
 sudo make install
 sudo /usr/local/bin/noip2
 sudo noip2 ­-S
-else
-echo ""
 fi }
