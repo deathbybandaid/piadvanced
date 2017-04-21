2ce8411df662442892d5514730a2e98dfdc4a67c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:26:36 -0400

    Update speedtestcli.sh

--- speedtestcli.sh
+++ speedtestcli.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
+## speedtest.cli
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install speedtest cli?" 8 78) then
 
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
