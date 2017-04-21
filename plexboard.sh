22191e63324647ca26eaffaba75bc592b8e612dd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 11:58:46 -0400

    Update plexboard.sh

--- plexboard.sh
+++ plexboard.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 ## Plexboard
-{ if (whiptail --yesno "Do you want to install Plexboard? Warning: this takes 15 minutes on a pi3, 2 hours on the pi1" 8 78) then
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Plexboard?" 8 78) then
+echo "User Declined Plexboard"
+else
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Warning: this takes 15 minutes on a pi3, 2 hours on the pi1" 8 78) then
+echo "User Declined Plexboard"
+else
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/pi/.rvm/scripts/rvm
 rvm requirements
@@ -9,6 +14,5 @@ git clone https://github.com/scytherswings/Plex-Board.git && cd /opt/Plex-Board
 rvm install 2.3.4 && rvm use 2.3.4@plexdashboard --create
 ./serverSetup.sh
 ./startServer.sh
-else
-echo ""
+fi }
 fi }
