2d4fdecf19d7e98db9fb9af59a269836c730b426
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:26:00 -0400

    Update sources.sh

--- sources.sh
+++ sources.sh
@@ -2,6 +2,9 @@
 ## Sources
 { whiptail --msgbox "I'm going to add sources ." 20 70 1 
 sudo mkdir /etc/piadvanced/backups/sources
+sudo sed -i '/deb http://repozytorium.mati75.eu/raspbian jessie-backports main contrib non-free/d' /etc/apt/sources.list
+sudo sed -i '/deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi/d' /etc/apt/sources.list.d/stretch.list
+sudo sed -i '/APT::Default-Release "jessie";/d' /etc/apt/apt.conf.d/99-default-release
 sudo cp /etc/apt/sources.list /etc/piadvanced/backups/sources/
 sudo cp /etc/apt/apt.conf.d/99-default-release /etc/piadvanced/backups/sources/
 sudo cp /etc/apt/sources.list.d/stretch.list /etc/piadvanced/backups/sources/
@@ -10,7 +13,7 @@ sudo echo 'deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib
 sudo echo 'APT::Default-Release "jessie";' | sudo tee --append /etc/apt/apt.conf.d/99-default-release
 sudo gpg --keyserver pgpkeys.mit.edu --recv-key CCD91D6111A06851
 sudo gpg --armor --export CCD91D6111A06851 | apt-key add -
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
 sudo wget https://archive.raspbian.org/raspbian.public.key -O - | sudo apt-key add -
-sudo gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
  }
