d0daef08ce7837e4a18f3137d714bb9608bcda77
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:29:53 -0400

    Update usermin.sh

--- usermin.sh
+++ usermin.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ## Usermin
-{ if (whiptail --yesno "Do you want to install Usermin?" 8 78) then
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Usermin?" 8 78) then
+echo "User Declined Usermin"
+else
 sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl
 sudo wget http://prdownloads.sourceforge.net/webadmin/usermin_1.701_all.deb
 sudo dpkg --install usermin_1.701_all.deb
 sudo echo "# Usermin" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "-A INPUT -p tcp --dport 20000 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
-else
-echo ""
 fi }
