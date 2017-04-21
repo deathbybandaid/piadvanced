fdfe5be6ad697e81a0491b422f5c3183da3774f8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:02:06 -0400

    Update psad.sh

--- psad.sh
+++ psad.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 ## PSAD
-{ if (whiptail --yesno "Do you want to install psad?" 8 78) then
-echo "Stay tuned"
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install psad?" 8 78) then
+echo "User Declined PSAD"
+else
 sudo wget http://cipherdyne.org/psad/download/psad-2.4.4.tar.gz -P /etc/piadvanced/installscripts/
 sudo tar xzf psad-2.4.4.tar.gz
 cd psad-2.4.4
 sudo ./install.pl
-else
-echo ""
 fi }
