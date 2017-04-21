ba51f1b3be3136e8c975d5952675658658f8fcee
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 14:31:14 -0400

    Update extendedinstall.sh

--- extendedinstall.sh
+++ extendedinstall.sh
@@ -32,11 +32,11 @@ sudo bash /etc/piadvanced/hostname.sh
 ## Static IP for eth0
 sudo bash /etc/piadvanced/eth0.sh
 
-## Staic IP for wlan0
-sudo bash /etc/piadvanced/wlan0.sh
-
 ## Wifi Credentials
 sudo bash /etc/piadvanced/wifissid.sh
+
+## Staic IP for wlan0
+sudo bash /etc/piadvanced/wlan0.sh
 
 ## Memory Split
 sudo bash /etc/piadvanced/memorysplit.sh
