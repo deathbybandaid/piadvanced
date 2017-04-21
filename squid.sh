ed50f4589dc11ea4bc2ccf293061c36d5f5f3e3e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:27:42 -0400

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,1 +1,29 @@
-## https://github.com/chr1x2/Squid-Proxy-Server-Install-Script-from-Source
+#!/bin/sh
+## Squid
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Squid?" 8 78) then
+
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+## Squidguard
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Squidguard?" 8 78) then
+
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
+fi }
+
