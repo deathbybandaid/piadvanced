ee3a91a1c29d2b072f0864f48a14bd4907c2f1d0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:21:41 -0400

    Update shellinabox.sh

--- shellinabox.sh
+++ shellinabox.sh
@@ -1,1 +1,15 @@
-# https://github.com/shellinabox/shellinabox
+#!/bin/sh
+## shellinabox
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install shellinabox?" 8 78) then
+
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
