cf79a2447e356a78eb5db736f46e00a3e3dca6c5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:00:58 -0400

    Update privoxy.sh

--- privoxy.sh
+++ privoxy.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+## Privoxy
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Privoxy?" 8 78) then
+
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
 sudo echo "# Privoxy" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "-A INPUT -p tcp --dport 8118 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+
+
+fi }
