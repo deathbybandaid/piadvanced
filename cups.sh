e400f92a4b9289306ca28b41925783d85af55f04
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 10:29:52 -0400

    Update cups.sh

--- cups.sh
+++ cups.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+## Cups
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Cups?" 8 78) then
+
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
 
