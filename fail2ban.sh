df47dc4ab7d8fb7a7f1a8aec4a5664dad08639ce
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 10:38:33 -0400

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ##Fail2Ban
-{ if (whiptail --yesno "Do you want to install fail2ban?" 8 78) then
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install fail2ban?" 8 78) then
+echo "User Declined fail2ban"
+else
 sudo apt-get install -y fail2ban
 sudo echo "# Fail2Ban" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "-A INPUT -p tcp -m multiport --dports 22 -j fail2ban-ssh" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "-A fail2ban-ssh -j RETURN" | sudo tee --append /etc/iptables.firewall.rules
 sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
-else
-echo ""
 fi }
