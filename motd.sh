50173527af9a1f1797f32458d445d70b0ec4cafa
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 10:59:50 -0400

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ## Message of the day
-{ if (whiptail --yesno "Do you want to Change your message of the day to a much cooler one?" 8 78) then
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Change your message of the day to a much cooler one?" 8 78) then
+echo "User Declined setting a cooler motd message"
+else
 whiptail --msgbox "This is the message you receive at login" 20 70 1
 sudo uname -snrvm > /var/run/motd.dynamic
 sudo systemctl disable motd
@@ -9,6 +11,4 @@ sudo rm -f /etc/motd
 sudo wget https://raw.githubusercontent.com/deathbybandaid/pimotd/master/10logo -P /etc/update-motd.d/
 chmod a+x /etc/update-motd.d/*
 sudo sed -i "s/motd.dynamic/motd.new/" /etc/pam.d/sshd
-else
-echo ""
 fi }
