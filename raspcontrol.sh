a7076f3c6a42e0a281d87e34bddfa926b0869d0f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 12:03:12 -0400

    Update raspcontrol.sh

--- raspcontrol.sh
+++ raspcontrol.sh
@@ -1,1 +1,15 @@
-https://github.com/harmon25/raspcontrol
+#!/bin/sh
+## raspcontrol
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install raspcontrol?" 8 78) then
+
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
