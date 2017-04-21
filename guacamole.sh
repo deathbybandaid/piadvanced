822571c112872539f2d5dac49b947242c4834491
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 10:41:27 -0400

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
+## Guacamole
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Guacamole?" 8 78) then
 
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
