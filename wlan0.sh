e076f64fd806145941f12b2c16107d7192639d67
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-21 11:19:54 -0400

    Create wlan0.sh

---
+++ wlan0.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+##
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set a static ip address for wlan0?" 8 78) then
+echo "User Declined Setting a Static IP Address for wlan0"
+else
+whiptail --msgbox "Let's set a static IP using wlan0" 20 70 1
+OLDWLAN_IP=`ip addr show wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
+OLDWLAN_GATEWAY=`ip route show 0.0.0.0/0 dev wlan0 | cut -d\  -f3`
+NEWWLAN_IP=$(whiptail --inputbox "Please enter desired IP for wlan0" 20 60 "$OLDWLAN_IP" 3>&1 1>&2 2>&3)
+sudo cp /etc/dhcpcd.conf /etc/piadvanced/backups/dhcpcd.conf
+sudo echo "" | sudo tee --append /etc/dhcpcd.conf
+sudo echo "#wlan0" | sudo tee --append /etc/dhcpcd.conf
+sudo echo "interface wlan0" | sudo tee --append /etc/dhcpcd.conf
+sudo echo "static ip_address=$NEWWLAN_IP" | sudo tee --append /etc/dhcpcd.conf
+sudo echo "static routers=$OLDWLAN_GATEWAY" | sudo tee --append /etc/dhcpcd.conf
+sudo echo "static domain_name_servers=$OLDWLAN_GATEWAY" | sudo tee --append /etc/dhcpcd.conf
+fi }
