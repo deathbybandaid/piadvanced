eaa0799eaa577beba1bfb267cb6d1932ba1dedb9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-25 12:34:54 -0400

    Update wifissid.sh

--- wifissid.sh
+++ wifissid.sh
@@ -12,7 +12,7 @@ sudo echo "NEW_PSK=$NEW_PSK" | sudo tee --append /etc/piadvanced/install/variabl
 sudo echo "" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
 sudo echo "## Wifi" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
 sudo echo "network={"| sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
-sudo echo "ssid="$NEW_SSID"" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
-sudo echo "psk=$NEW_PSK" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo "    ssid="$NEW_SSID"" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo "    psk=$NEW_PSK" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
 sudo echo "}" | sudo tee --append /etc/wpa_supplicant/wpa_supplicant.conf
 fi }
