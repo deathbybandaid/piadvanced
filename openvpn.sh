04f381d9c5aee675113b652ff65f7dbc657349f8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-26 15:06:44 -0400

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -8,9 +8,6 @@ whiptail --msgbox "Select TCP during the install" 20 70 1
 sudo wget https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -P /etc/piadvanced/installscripts/
 sudo chmod +x /etc/piadvanced/installscripts/openvpn-install.sh
 sudo bash /etc/piadvanced/installscripts/openvpn-install.sh
-sudo echo "# VPN" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "-A INPUT -p tcp --dport 1194 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "openvpnfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 sudo sed -i "s/push "dhcp-option DNS 8.8.8.8"/"dhcp-option DNS 10.8.0.1"/" /etc/openvpn/server.conf
-sudo echo "OPENVPN_NAT=-A POSTROUTING -s 10.8.0.0/24 -j SNAT --to-source $NEWETH_IP" | sudo tee --append /etc/piadvanced/install/variables.conf
 fi }
