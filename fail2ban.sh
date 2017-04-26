959cbc0c0d8e91c06f6e992b68cee75c734001e1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-26 14:57:23 -0400

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -4,8 +4,5 @@
 echo "User Declined fail2ban"
 else
 sudo apt-get install -y fail2ban
-sudo echo "# Fail2Ban" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "-A INPUT -p tcp -m multiport --dports 22 -j fail2ban-ssh" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "-A fail2ban-ssh -j RETURN" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "fail2banfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
