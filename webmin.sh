c1a9c67c787a37d49ab6755c3afe5ca405f8a123
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-26 15:34:14 -0400

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -6,7 +6,5 @@ else
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
 sudo dpkg --install webmin_1.831_all.deb
-sudo echo "# Webmin" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "-A INPUT -p tcp --dport 10000 -j ACCEPT" | sudo tee --append /etc/iptables.firewall.rules
-sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "webminfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
