98a2d9ef0d43c7a7404751e5e064e16b4b35d4b9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-26 19:33:25 -0400

    Create Extendedclone.sh

---
+++ Extendedclone.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+
+## THIS SCRIPT IS NOT READY!!!
+## This script is similar to the reinstall script, but asks about network interface questions.
+## It will be handy for setting things up for friends
