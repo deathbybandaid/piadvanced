5e3e3b730d101bd3b990e26f592dca0dd11f2aae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-26 19:38:08 -0400

    Update dnscrypt.sh

--- dnscrypt.sh
+++ dnscrypt.sh
@@ -8,31 +8,27 @@ sudo apt-get -y install build-essential tcpdump dnsutils libsodium-dev
 sudo apt-get -y install locate bash-completion libsystemd-dev pkg-config
 mkdir -p dnsproxy
 cd dnsproxy
-wget https://download.dnscrypt.org/dnscrypt-proxy/LATEST.tar.bz2
-tar -xf LATEST.tar.bz2
-rm LATEST.tar.bz2
+sudo get https://download.dnscrypt.org/dnscrypt-proxy/LATEST.tar.bz2
+sudo tar -xf LATEST.tar.bz2
+sudo rm LATEST.tar.bz2
 pattern="dnscrypt-proxy-"
 for _dir in *"${pattern}"*; do     [ -d "${_dir}" ] && dir="${_dir}" && break; done
 cd $dir
 sudo ldconfig
-./configure --with-systemd
-make
+sudo bash ./configure --with-systemd
+sudo make
 sudo make install
 sudo useradd -r -d /var/dnscrypt -m -s /usr/sbin/nologin dnscrypt
-echo Done
-echo Adding Resolver Processes To rc.local
 sudo sed -i '/exit 0/d' /etc/rc.local
 sudo sed -i '/dnscrypt-proxy/d' /etc/rc.local
 sudo sed -i '$i /usr/local/sbin/dnscrypt-proxy --resolver-name=dnscrypt.eu-dk --user=dnscrypt -a 127.0.0.2:5454 --edns-payload-size=4096 --logfile=/var/log/dnscrypt-proxy.1.log &> /dev/null &' /etc/rc.local
 sudo sed -i '$i /usr/local/sbin/dnscrypt-proxy --resolver-name=cs-ch --user=dnscrypt -a 127.0.0.3:5656 --edns-payload-size=4096 --logfile=/var/log/dnscrypt-proxy.2.log &> /dev/null &' /etc/rc.local
 sudo sed -i '$i /usr/local/sbin/dnscrypt-proxy --resolver-name=d0wn-is-ns1 --user=dnscrypt -a 127.0.0.4:5757 --edns-payload-size=4096 --logfile=/var/log/dnscrypt-proxy.3.log &> /dev/null &' /etc/rc.local
 sudo sed -i '$i exit 0' /etc/rc.local
-echo Starting Resolvers Manually (Will load via rc.local after reboot)
 sudo kill $(ps aux | grep 'dnscrypt-proxy' | awk '{print $2}')
 sudo /usr/local/sbin/dnscrypt-proxy --resolver-name=dnscrypt.eu-dk --user=dnscrypt -a 127.0.0.2:5454 --edns-payload-size=4096 --logfile=/var/log/dnscrypt-proxy.1.log &> /dev/null &
 sudo /usr/local/sbin/dnscrypt-proxy --resolver-name=cs-ch --user=dnscrypt -a 127.0.0.3:5656 --edns-payload-size=4096 --logfile=/var/log/dnscrypt-proxy.2.log &> /dev/null &
 sudo /usr/local/sbin/dnscrypt-proxy --resolver-name=d0wn-is-ns1 --user=dnscrypt -a 127.0.0.4:5757 --edns-payload-size=4096 --logfile=/var/log/dnscrypt-proxy.3.log &> /dev/null &
-echo Adding Entries To Host File
 sudo sed -i '/dnscrypt.eu/d' /etc/hosts
 sudo sed -i '/cs-ch/d' /etc/hosts
 sudo sed -i '/d0wn-is-ns1/d' /etc/hosts
@@ -41,11 +37,7 @@ sudo sed -i '$i 127.0.0.3       cs-ch' /etc/hosts
 sudo sed -i '$i 127.0.0.4       d0wn-is-ns1' /etc/hosts
 echo Adding Entries to DNSMasq
 sudo /etc/init.d/dnsmasq stop
-#(crontab -l ; echo "0 5 * * 1 sudo sed -i '/server=/d' /etc/dnsmasq.d/01-pihole.conf") | crontab -
 sudo sed -i '/server=/d' /etc/dnsmasq.d/01-pihole.conf
-#sudo sed -i '$i server=127.0.0.2#5454' /etc/dnsmasq.d/01-pihole.conf
-#sudo sed -i '$i server=127.0.0.3#5656' /etc/dnsmasq.d/01-pihole.conf
-#sudo sed -i '$i server=127.0.0.4#5757' /etc/dnsmasq.d/01-pihole.conf
 sudo /etc/init.d/dnsmasq start
 ps aux | grep 'dnscrypt-proxy'
 echo Done
