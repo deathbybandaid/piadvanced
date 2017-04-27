234602033b34f66be93a0f5f67afe94335dd1aa5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 09:24:55 -0400

    Update cups.sh

--- cups.sh
+++ cups.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 ## Cups
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Cups?" 8 78) then
-
-
-
-echo "not installing stuff"
-
+echo "User Declined CUPS"
 else
-
-echo "installing stuff"
-
-
-
+sudo apt-get install -y cups
+sudo usermod -a -G lpadmin pi
+sudo sed -i "s/Listen localhost:631/Port 631/" /etc/cups/cupsd.conf
+sed "/'Order allow,deny'/a 'Allow from all'" /etc/cups/cupsd.conf
+sudo /etc/init.d/cups restart
+sudo echo "cupsfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
-
