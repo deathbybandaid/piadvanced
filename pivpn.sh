22ffd7ff0b5965203d8815fdbf8225daba64e1b2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 08:49:37 -0400

    Update pivpn.sh

--- pivpn.sh
+++ pivpn.sh
@@ -1,2 +1,7 @@
-
+#!/bin/sh
+## Apticron
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install pivpn?" 8 78) then
+echo "User Declined pivpn"
+else
 curl -L https://install.pivpn.io | bash
+fi }
