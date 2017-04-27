4fe90b283ecf90b64bcffe202f3a1ef191179308
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 17:46:55 -0400

    Update privoxy.sh

--- privoxy.sh
+++ privoxy.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 ## Privoxy
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Privoxy?" 8 78) then
-
-
-
-echo "not installing stuff"
-
+echo "User Declined Privoxy"
 else
-
-echo "installing stuff"
-
+sudo apt-get install -y privoxy
 sudo echo "privoxyfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
-
 fi }
