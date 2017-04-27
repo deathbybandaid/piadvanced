133b37d68311230abcfd2a65d24884bcb7b42f0b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 09:02:31 -0400

    Update ddclient.sh

--- ddclient.sh
+++ ddclient.sh
@@ -1,1 +1,7 @@
-# https://sourceforge.net/p/ddclient/wiki/Home/
+#!/bin/sh
+## ddclient
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install ddclient?" 8 78) then
+echo "User Declined ddclient"
+else
+sudo apt-get install -y ddclient
+fi }
