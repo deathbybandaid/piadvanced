f36c7ac118ee02829f89854d2884d2bf18e81101
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 13:00:18 -0400

    Update cups.sh

--- cups.sh
+++ cups.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 ## Cups
-source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/userchange.conf
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Cups?" 8 78) then
 echo "User Declined CUPS"
 else
 sudo apt-get install -y cups
-sudo usermod -a -G lpadmin pi
+{ if [ "$CHANGED_USERNAME" = "yes" ]
+then
 sudo usermod -a -G lpadmin $NEW_USERNAME
+else
+sudo usermod -a -G lpadmin pi
+fi }
 sudo sed -i "s/Listen localhost:631/Port 631/" /etc/cups/cupsd.conf
 sudo gawk -i inplace '/Order deny,allow/{print;print "Allow from all";next}1' /etc/cups/cupsd.conf
 sudo gawk -i inplace '/Order allow,deny/{print;print "Allow from all";next}1' /etc/cups/cupsd.conf
