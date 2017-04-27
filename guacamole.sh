d9f358b5aebd953f17c026ca4c6fb3f513776c1b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 09:47:23 -0400

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 ## Guacamole
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Guacamole?" 8 78) then
-
-
-
-echo "not installing stuff"
-
+echo "User Declined Guacamole"
 else
-
-echo "installing stuff"
-
-
-
+wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/master/guac-install.sh -P /etc/piadvanced/installscripts/
+sudo apt-get install -y dos2unix
+sudo dos2unix /etc/piadvanced/installscripts/guac-install.sh
+sudo bash /etc/piadvanced/installscripts/guac-install.sh
 fi }
