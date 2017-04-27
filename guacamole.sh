340fc3afd7ed628d9b14bf3a4662319799dd6a90
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 13:07:41 -0400

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -4,6 +4,7 @@
 echo "User Declined Guacamole"
 else
 wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/master/guac-install.sh -P /etc/piadvanced/installscripts/
+chmod +x /etc/piadvanced/installscripts/guac-install.sh
 sudo apt-get install -y dos2unix
 sudo dos2unix /etc/piadvanced/installscripts/guac-install.sh
 sudo bash /etc/piadvanced/installscripts/guac-install.sh
