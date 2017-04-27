2729baaf5a1c6b88f695836a8819d69b6eed3c64
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-27 12:39:07 -0400

    Update cups.sh

--- cups.sh
+++ cups.sh
@@ -6,7 +6,11 @@ else
 sudo apt-get install -y cups
 sudo usermod -a -G lpadmin pi
 sudo sed -i "s/Listen localhost:631/Port 631/" /etc/cups/cupsd.conf
-sed "/'Order allow,deny'/a 'Allow from all'" /etc/cups/cupsd.conf
+sudo gawk -i inplace '/Order deny,allow/{print;print "Allow from all";next}1' /etc/cups/cupsd.conf
+sudo gawk -i inplace '/Order allow,deny/{print;print "Allow from all";next}1' /etc/cups/cupsd.conf
+#sudo awk '/Order deny,allow/{print;print "Allow from all";next}1' /etc/cups/cupsd.conf
+#sudo awk '/Order allow,deny/{print;print "Allow from all";next}1' /etc/cups/cupsd.conf
+#sed "/'Order allow,deny'/a 'Allow from all'" /etc/cups/cupsd.conf
 sudo /etc/init.d/cups restart
 sudo echo "cupsfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
