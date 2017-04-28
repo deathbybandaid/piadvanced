e1a1a08636c484059ce12fec3b08c69489680d5b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-28 08:07:39 -0400

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -3,11 +3,10 @@
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Guacamole?" 8 78) then
 echo "User Declined Guacamole"
 else
-sudo wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/master/guac-install.sh -P /etc/piadvanced/installscripts/
-sudo chmod +x /etc/piadvanced/installscripts/guac-install.sh
-sudo apt-get install -y dos2unix
-sudo dos2unix /etc/piadvanced/installscripts/guac-install.sh
-sudo bash /etc/piadvanced/installscripts/guac-install.sh
+sudo wget https://raw.githubusercontent.com/deathbybandaid/piguac/master/guac.sh -P /etc/piadvanced/installscripts/
+sudo chmod +x /etc/piadvanced/installscripts/guac.sh
+sudo dos2unix /etc/piadvanced/installscripts/guac.sh
+sudo bash /etc/piadvanced/installscripts/guac.sh
 sudo echo "guacamolefirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
 
