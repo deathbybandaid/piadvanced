95e0a5e0af87c21d9d270b965fa97ed17e09d648
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-28 12:22:56 -0400

    Update EXAMPLEMODULE.sh

--- EXAMPLEMODULE.sh
+++ EXAMPLEMODULE.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
-##
+## NAMEOFAPP
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install ?" 8 78) then
 echo "User Declined NAMEOFAPP"
 
 else
+## Need a message box?
+whiptail --msgbox "NAMEOFAPP messagebox" 20 70 1
+
+## Set a variable with a text box?
+NEW_VARIABLE=$(whiptail --inputbox "NAMEOFAPP what do you want to type?" 20 60 "NAMEOFAPP suggested text" 3>&1 1>&2 2>&3)
+
 
 echo "installing stuff"
 
+## Execute install script
+sudo bash /etc/piadvanced/installscripts/NAMEOFAPP.sh
 
-## If Firewall rule is needed, replace name of app.
-#sudo echo "NAMEOFAPPfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+## If Firewall rule is needed
+sudo echo "NAMEOFAPPfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 
+## Save variable to the conf
+sudo echo "NEW_VARIABLE=$NEW_VARIABLE" | sudo tee --append /etc/piadvanced/install/variables.conf
 fi }
