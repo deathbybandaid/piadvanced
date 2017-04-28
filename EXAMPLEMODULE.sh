9b7417735f68c739763cb88c3f782e462055aa4f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-28 12:10:58 -0400

    Update EXAMPLEMODULE.sh

--- EXAMPLEMODULE.sh
+++ EXAMPLEMODULE.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 ##
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install ?" 8 78) then
-
-
-
-echo "not installing stuff"
+echo "User Declined NAMEOFAPP"
 
 else
 
 echo "installing stuff"
 
 
+## If Firewall rule is needed, replace name of app.
+#sudo echo "NAMEOFAPPfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 
 fi }
