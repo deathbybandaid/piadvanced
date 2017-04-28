51ca1788070fc8d1ebe4c62d56970841a7f6f51e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-28 09:25:51 -0400

    Delete privoxy.sh

--- privoxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-## Privoxy
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Privoxy?" 8 78) then
-echo "User Declined Privoxy"
-else
-sudo apt-get install -y privoxy
-sudo echo "privoxyfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
-fi }
