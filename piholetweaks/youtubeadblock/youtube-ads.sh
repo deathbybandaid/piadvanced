9c238e313d7dd6fcdac0fd9f2db7e220e1ff9aae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-28 10:52:37 -0400

    Update youtube-ads.sh

--- piholetweaks/youtubeadblock/youtube-ads.sh
+++ piholetweaks/youtubeadblock/youtube-ads.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ## Youtube adblocking
-rm /var/www/html/admin/youtube.txt
-rm /etc/piadvanced/installscripts/youtubeadblock/youtube-domains.txt
-rm /etc/piadvanced/installscripts/youtubeadblock/youtube-filtered.txt
-rm /etc/piadvanced/installscripts/youtubeadblock/youtube-ads.txt
-python /etc/piadvanced/installscripts/youtubeadblock/API_example.py > /etc/piadvanced/installscripts/youtubeadblock/youtube-domains.txt
-grep "^r" /etc/piadvanced/installscripts/youtubeadblock/youtube-domains.txt > /etc/piadvanced/installscripts/youtubeadblock/youtube-filtered.txt
-sed 's/\s.*$//' /etc/piadvanced/installscripts/youtubeadblock/youtube-filtered.txt > /etc/piadvanced/installscripts/youtubeadblock/youtube-ads.txt
-cp /etc/piadvanced/installscripts/youtubeadblock/youtube-ads.txt /var/www/html/admin/youtube.txt
+sudo rm /var/www/html/admin/youtube.txt
+sudo rm /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-domains.txt
+sudo rm /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-filtered.txt
+sudo rm /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-ads.txt
+sudo python /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/API_example.py > /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-domains.txt
+sudo grep "^r" /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-domains.txt > /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-filtered.txt
+sudo sed 's/\s.*$//' /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-filtered.txt > /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-ads.txt
+sudo cp /etc/piadvanced/piholetweaks/youtubeadblock/youtubeadblock/youtube-ads.txt /var/www/html/admin/youtube.txt
 #greps the log for youtube ads and appends to /var/www/html/admin/youtube.txt
-grep r*.googlevideo.com /var/log/pihole.log | awk '{print $6}'| grep -v '^googlevideo.com\|redirector' | sort -nr | uniq >> /var/www/html/admin/youtube.txt
+sudo grep r*.googlevideo.com /var/log/pihole.log | awk '{print $6}'| grep -v '^googlevideo.com\|redirector' | sort -nr | uniq >> /var/www/html/admin/youtube.txt
 #removes duplicate lines from /var/www/html/youtube.txt
-perl -i -ne 'print if ! $x{$_}++' /var/www/html/admin/youtube.txt
+sudo perl -i -ne 'print if ! $x{$_}++' /var/www/html/admin/youtube.txt
 #updates pihole blacklist/whitelist
 #pihole -g
