02bda66b7865d86d7d7ce9ee118b8a211c343909
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-28 10:02:34 -0400

    Delete Extendedclone.sh

--- Extendedclone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-
-## THIS SCRIPT IS NOT READY!!!
-## This script is similar to the reinstall script, but asks about network interface questions.
-## It will be handy for setting things up for friends
