e69cc73262c17723582f14fad919eb74a305888a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-28 08:04:42 -0400

    Update aptgetinstall.sh

--- aptgetinstall.sh
+++ aptgetinstall.sh
@@ -53,7 +53,29 @@ sudo apt-get install -y lsof
 sudo apt-get install -y netcat
 sudo apt-get install -y sudo
 sudo systemctl enable php7.0-fpm
+sudo apt-get -t stretch -y install libcairo2-dev
+sudo apt-get -t stretch -y install libjpeg-turbo8-dev
+sudo apt-get -t stretch -y install libpng12-dev
+sudo apt-get -t stretch -y install libossp-uuid-dev
+sudo apt-get -t stretch -y install libavcodec-dev
+sudo apt-get -t stretch -y install libavutil-dev
+sudo apt-get -t stretch -y install libswscale-dev
+sudo apt-get -t stretch -y install libfreerdp-dev
+sudo apt-get -t stretch -y install libpango1.0-dev
+sudo apt-get -t stretch -y install libssh2-1-dev
+sudo apt-get -t stretch -y install libtelnet-dev
+sudo apt-get -t stretch -y install libvncserver-dev
+sudo apt-get -t stretch -y install libpulse-dev
+sudo apt-get -t stretch -y install libssl-dev
+sudo apt-get -t stretch -y install libvorbis-dev
+sudo apt-get -t stretch -y install libwebp-dev
+sudo apt-get -t stretch -y install mysql-server
+sudo apt-get -t stretch -y install mysql-client
+sudo apt-get -t stretch -y install mysql-common
+sudo apt-get -t stretch -y install mysql-utilities
+sudo apt-get -t stretch -y install tomcat8
+sudo apt-get -t stretch -y install freerdp
+sudo apt-get -t stretch -y install ghostscript
+sudo apt-get -t stretch -y install jq
  }
 fi }
-
-
