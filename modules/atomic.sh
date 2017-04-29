0ba5cab151615c95e5a42ad18f8249a294a60af3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 16:22:49 -0400

    Create atomic.sh

---
+++ modules/atomic.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## AtoMiC-ToolKit
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the AtoMiC-ToolKit for htpc softwares?" 8 78) then
+echo "User Declined Atomic"
+else
+sudo git clone https://github.com/htpcBeginner/AtoMiC-ToolKit ~/AtoMiC-ToolKit
+sudo bash ~/AtoMiC-ToolKit/setup.sh
+whiptail --msgbox "Any programs installed via Atomic need firewall rules." 20 70 1
+fi }
