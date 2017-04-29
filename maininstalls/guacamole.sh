53139e11e0fcd10aee58bd45b2ff98c604883ab3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 16:45:05 -0400

    Delete guacamole.sh

--- maininstalls/guacamole.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## Guacamole
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Guacamole?" 8 78) then
-echo "User Declined Guacamole"
-else
-sudo dos2unix /etc/piadvanced/installscripts/guacamole.sh
-sudo bash /etc/piadvanced/installscripts/guacamole.sh
-sudo echo "guacamolefirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
-fi }
