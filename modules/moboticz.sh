f7a72ccdda7b6c11232801c5645854a853fff85c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 16:27:47 -0400

    Create moboticz.sh

---
+++ modules/moboticz.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+## moboticz
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install moboticz?" 8 78) then
+
+
+
+echo "not installing stuff"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
