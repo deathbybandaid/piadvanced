38bfdb662a2fa483c3cf6ee2e1837a9eb44dddf0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 16:44:49 -0400

    Delete exim4.sh

--- maininstalls/exim4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-## exim4
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install exim4?" 8 78) then
-echo "User Declined Exim4"
-else
-sudo apt-get install -y exim4 
-sudo dpkg-reconfigure exim4-config
-fi }
