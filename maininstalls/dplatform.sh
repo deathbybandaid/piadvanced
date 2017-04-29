53062fd2ecbb7f8857c2da370e7f6764442b03e3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 12:28:16 -0400

    Create dplatform.sh

---
+++ maininstalls/dplatform.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## Dplatform
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the Dplatform installer?" 8 78) then
+echo "User Declined Dplatform"
+else
+sudo wget https://raw.githubusercontent.com/DFabric/DPlatform-ShellCore/master/init.sh -P /etc/piadvanced/installscripts/dplatform/
+sudo bash /etc/piadvanced/installscripts/dplatform/init.sh
+whiptail --msgbox "Any programs installed via Dplatform need firewall rules." 20 70 1
+fi }
