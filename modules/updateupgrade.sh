dd9eacceb34ff46bb7f87ed8fe96a4fb7014173d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 16:53:15 -0400

    Update updateupgrade.sh

--- modules/updateupgrade.sh
+++ modules/updateupgrade.sh
@@ -1,9 +1,9 @@
 #! / Bin / sh
 ## Updates
-{Whiptail --msgbox "I'm going to run updates." 20 70 1
-Sudo apt-get -y install
-Sudo apt-get -y update --fix-missing
-Sudo apt-get -y dist-upgrade -alow-downgrades
-Sudo apt-get -y autoremove
-Sudo apt-get clean
+{whiptail --msgbox "I'm going to run updates." 20 70 1
+sudo apt-get -y install
+sudo apt-get -y update --fix-missing
+sudo apt-get -y dist-upgrade -alow-downgrades
+sudo apt-get -y autoremove
+sudo apt-get clean
  }
