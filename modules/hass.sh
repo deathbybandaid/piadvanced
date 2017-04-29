ad6ef2fb4d1c6065499d661f27a32ca7f2684e4b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 19:47:50 -0400

    Update hass.sh

--- modules/hass.sh
+++ modules/hass.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 ## HASS
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install HASS?" 8 78) then
-
-
-
-echo "not installing stuff"
-
+echo "User Declined HASS"
 else
-
-echo "installing stuff"
-
-
-
+curl -O https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/hass_rpi_installer.sh && sudo chown pi:pi hass_rpi_installer.sh && bash hass_rpi_installer.sh
+sudo echo "hassfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
