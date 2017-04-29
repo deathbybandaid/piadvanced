d87cb9abeaf958fbf428b4749ee4adb9e448ad87
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-04-29 16:44:27 -0400

    Delete dnscrypt.sh

--- maininstalls/dnscrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## DNSCRYPT
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install DNSCrypt?" 8 78) then
-echo "User Declined DNSCrypt"
-else
-sudo bash /etc/piadvanced/installscripts/dnsproxy/dnscryptinstall.sh
-sudo echo "dnscryptfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
-sudo cp /etc/piadvanced/piholetweaks/dnscrypt/10-dnscrypt.conf /etc/dnsmasq.d/
-fi }
