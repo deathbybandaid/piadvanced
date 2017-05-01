68861018dcc93f00b0621929623c8a30565c6449
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-01 12:09:07 -0400

    Update hass.sh

--- modules/hass.sh
+++ modules/hass.sh
@@ -4,6 +4,12 @@
 echo "User Declined HASS"
 else
 sudo wget https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/hass_rpi_installer.sh -P  /etc/piadvanced/installscripts/
-sudo bash /etc/piadvanced/installscripts/hass_rpi_installer.sh
+{ if [ "$CHANGED_USERNAME" = "yes" ]
+then
+sudo chown $NEW_USERNAME:$NEW_USERNAME /etc/piadvanced/installscripts/hass_rpi_installer.sh
+else
+sudo chown pi:pi /etc/piadvanced/installscripts/hass_rpi_installer.sh
+fi }
+bash /etc/piadvanced/installscripts/hass_rpi_installer.sh
 sudo echo "hassfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
