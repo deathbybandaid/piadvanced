586fb509e757d0508d31fd82b4f4f01439860287
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:01:44 -0400

    Update pihole-stalelist.sh

--- modules/piholetweakmodules/pihole-stalelist.sh
+++ modules/piholetweakmodules/pihole-stalelist.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a script to keep cached lists from becoming stale?" 10 80) 
+(whiptail --title "Stale List Fix" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a script to keep cached lists from becoming stale?" 10 80) 
 then
 echo "User Declined Stale List Fix"
 else
