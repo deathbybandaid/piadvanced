56726a5bdc2353c95119625c8db69d738e4ec8cd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:18:11 -0400

    Update openvas.sh

--- modules/openvas.sh
+++ modules/openvas.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Openvas
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install openvas?" 8 78) then
 
 
