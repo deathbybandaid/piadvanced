6d7b31d63a51ccd0d7e8289c021ba9a0c1da73bd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:17:25 -0400

    Update noip.sh

--- modules/noip.sh
+++ modules/noip.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## No-IP
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using the No-IP dynamic Update Client?" 8 78) then
 echo "User Declined NOIP DUC"
 else
