64695e09d91241ee7979938d3be2a9a93e975e24
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:00:22 -0400

    Update pihole-deathbybandaid.sh

--- modules/piholetweakmodules/pihole-deathbybandaid.sh
+++ modules/piholetweakmodules/pihole-deathbybandaid.sh
@@ -6,9 +6,8 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install deathbybandaid dnsmasq tweaks? See readme for more information." 8 78) 
+(whiptail --title "Deathbybandaid dnsmasq tweaks" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install deathbybandaid dnsmasq tweaks? See readme for more information." 8 78) 
 then
 echo "User Declined deathbybandaid dnsmasq tweaks"
 else
