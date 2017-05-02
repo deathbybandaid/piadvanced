b041a903fb7215ce8d23a76b100c26bb3ab3002c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:33:25 -0400

    Update ssh.sh

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -7,15 +7,19 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 ## I might add the option to use a key versus password for login.
-{ whiptail --yesno "Would you like the SSH server enabled or disabled?" 20 60 2 \
+{ whiptail --yesno "Would you like the SSH server enabled or disabled?" 10 80 2 \
 --yes-button Enable --no-button Disable
 RET=$?
-if [ $RET -eq 0 ]; then
+if 
+[ $RET -eq 0 ]; 
+then
 update-rc.d ssh enable &&
 invoke-rc.d ssh start &&
 whiptail --msgbox "SSH server enabled" 20 60 1
 sudo echo "sshfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
-elif [ $RET -eq 1 ]; then
+elif 
+[ $RET -eq 1 ]; 
+then
 update-rc.d ssh disable &&
 whiptail --msgbox "SSH server disabled" 20 60 1
 else
