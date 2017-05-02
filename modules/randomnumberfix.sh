d360101cf95a19f764836accb041a57477bb51e5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:19:25 -0400

    Update randomnumberfix.sh

--- modules/randomnumberfix.sh
+++ modules/randomnumberfix.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Random Number fix
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you wnat to install the random number fix?" 8 78) then
 echo "User Declined RNG"
 else
