c7ab682b1f86a2330d88565ff20be8aa93e0ccb7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 16:22:43 -0400

    Update pihole-wally3kadlists.sh

--- modules/piholetweakmodules/pihole-wally3kadlists.sh
+++ modules/piholetweakmodules/pihole-wally3kadlists.sh
@@ -6,12 +6,10 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install the Wally3k adlists.list?" 8 78) then
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Would you like to copy adlists.default to adlists.list instead?" 8 78) then
-echo "User Declined Using adlists.list"
-else
-sudo cp /etc/pihole/adlists.default /etc/pihole/adlists.list
-fi }
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install the Wally3k adlists.list?" 10 80) 
+then
+echo "User Declined Wally3k's adlists.list"
 else
 sudo wget https://raw.githubusercontent.com/deathbybandaid/wally3k-adlists.list/master/adlists.list -P /etc/pihole/
 fi }
