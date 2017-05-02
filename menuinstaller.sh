63352bf08d7c7519ec9f301555188d65e6231aa5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 15:35:35 -0400

    Update menuinstaller.sh

--- menuinstaller.sh
+++ menuinstaller.sh
@@ -1,1 +1,8 @@
-
+whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
+"<-- Back" "Return to the main menu." \
+"Add User" "Add a user to the system." \
+"Modify User" "Modify an existing user." \
+"List Users" "List all users on the system." \
+"Add Group" "Add a user group to the system." \
+"Modify Group" "Modify a group and its list of members." \
+"List Groups" "List all groups on the system."
