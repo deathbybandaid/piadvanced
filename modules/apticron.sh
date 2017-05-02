bd377d671b6d2c96ff6522ce3331f7732ab4d71b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:11:04 -0400

    Update apticron.sh

--- modules/apticron.sh
+++ modules/apticron.sh
@@ -6,10 +6,12 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install apticron to recieve updates when you have pending updates?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install apticron to recieve updates when you have pending updates?" 10 80) 
+then
 echo "User Declined Apticron"
 else
-APTICRON_EMAIL=$(whiptail --inputbox "What email address do you want to use?" 20 60 "user@domain.net" 3>&1 1>&2 2>&3)
+APTICRON_EMAIL=$(whiptail --inputbox "What email address do you want to use?" 10 80 "user@domain.net" 3>&1 1>&2 2>&3)
 sudo apt-get install apticron
 sudo sed -i "s/EMAIL="root"/EMAIL="$APTICRON_EMAIL"/" /etc/apticron/apticron.conf
 fi }
