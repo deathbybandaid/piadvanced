c532687bd20562e0334c5cfe672fe6c3774bddd2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:16:31 -0400

    Update mail.sh

--- modules/mail.sh
+++ modules/mail.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 ## Mail
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to recieve emails regarding successful cron jobs?" 8 78) then
 echo "User Delined Mail"
 else
