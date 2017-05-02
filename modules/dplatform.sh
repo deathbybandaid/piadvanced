e384adca505e2a1a04126d88b55a2f176a9df87a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:14:02 -0400

    Update dplatform.sh

--- modules/dplatform.sh
+++ modules/dplatform.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Dplatform
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the Dplatform installer?" 8 78) then
 echo "User Declined Dplatform"
 else
