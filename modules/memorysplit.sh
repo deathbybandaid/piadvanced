d92afc69139e02ab0c64f167d9fc5ffdc24f7ecf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:20:56 -0400

    Update memorysplit.sh

--- modules/memorysplit.sh
+++ modules/memorysplit.sh
@@ -6,20 +6,24 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the memory split" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the memory split" 10 80) 
+then
 sudo sed -i '/total_mem/ d' /boot/config.txt
 else
-NEWMEM_SPLIT=$(whiptail --inputbox "Do you plan on running headless? If so, set the memory split to 16." 20 60 "16" 3>&1 1>&2 2>&3)
+NEWMEM_SPLIT=$(whiptail --inputbox "Do you plan on running headless? If so, set the memory split to 16." 10 80 "16" 3>&1 1>&2 2>&3)
 sudo cp /boot/config.txt /etc/piadvanced/backups/boot/
 sudo sed -i '/gpu_mem/ d' /boot/config.txt
 sudo echo "gpu_mem=$NEWMEM_SPLIT" | sudo tee --append /boot/config.txt
 fi }
 
 ## This is an experimental tweak to unlock the pi's missing 16MB
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to try the experimental unlock of an extra 16MB. This is for the Pi2 and Pi3 only" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to try the experimental unlock of an extra 16MB. This is for the Pi2 and Pi3 only" 10 80) 
+then
 sudo sed -i '/total_mem/ d' /boot/config.txt
 else
-NEWMEM_UNLOCK=$(whiptail --inputbox "Setting to 1024 is the Maximum. It may be prudent to say 1023" 20 60 "1023" 3>&1 1>&2 2>&3)
+NEWMEM_UNLOCK=$(whiptail --inputbox "Setting to 1024 is the Maximum. It may be prudent to say 1023" 10 80 "1023" 3>&1 1>&2 2>&3)
 sudo sed -i '/total_mem/ d' /boot/config.txt
 sudo echo "total_mem=$NEWMEM_UNLOCK" | sudo tee --append /boot/config.txt
 fi }
