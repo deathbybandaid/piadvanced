22a0630d111ee371b7ffcea33111337dd03079cb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 15:10:24 -0400

    Update removedefaultpiuser.sh

--- removedefaultpiuser.sh
+++ removedefaultpiuser.sh
@@ -17,11 +17,9 @@ This will make the pi a great deal more secure.
 I will ask you for a username and a password.
 " 20 70 1
 NEW_USERNAME=$(whiptail --title "New Username" --inputbox "Please enter desired username, you will then be prompted to create a new password" 20 60 "" 3>&1 1>&2 2>&3)
-echo "$NEW_USERNAME" 
-fi }
-
 usermod -l $NEW_USERNAME -m -d /home/$NEW__USERNAME pi
 groupmod -n $NEW_USERNAME pi
 sudo passwd $NEW_USERNAME
 sudo echo "NEW_USERNAME=$NEW_USERNAME" | sudo tee --append /etc/piadvanced/install/userchange.conf
-sudo echo "CHANGED_USERNAME=yes" | sudo tee --append /etc/piadvanced/install/userchange.conf
+sudo echo "CHANGED_USERNAME=yes" | sudo tee --append /etc/piadvanced/install/userchange.conf 
+fi }
