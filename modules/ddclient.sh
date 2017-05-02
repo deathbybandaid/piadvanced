168fc1761ff040b8110e00489ab1505090eff620
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:12:35 -0400

    Update ddclient.sh

--- modules/ddclient.sh
+++ modules/ddclient.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install ddclient?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install ddclient?" 10 80) 
+then
 echo "User Declined ddclient"
 else
 sudo apt-get install -y ddclient
