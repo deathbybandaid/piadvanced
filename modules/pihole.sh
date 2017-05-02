6b30ab5244b6eefcc64e3dc48bc0e49e34c81195
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:18:34 -0400

    Update pihole.sh

--- modules/pihole.sh
+++ modules/pihole.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 ## pihole
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install pihole?" 8 78) then
 echo "User Declined Pi-Hole"
