bdd8397b82d003b8f5fa07d66fced454c5e45f9e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:12:48 -0400

    Update aptgetinstall.sh

--- modules/aptgetinstall.sh
+++ modules/aptgetinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+## Dependencies
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
 
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Basic Software dependencies?" 8 78) then
 echo "User Declined Dependencies"
