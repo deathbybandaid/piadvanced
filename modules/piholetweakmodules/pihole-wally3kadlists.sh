ae9848d029265a3eabc269c2a064f7cb4f3f5652
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:02:47 -0400

    Update pihole-wally3kadlists.sh

--- modules/piholetweakmodules/pihole-wally3kadlists.sh
+++ modules/piholetweakmodules/pihole-wally3kadlists.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install the Wally3k adlists.list?" 10 80) 
+(whiptail --title "Wally3k adlists.list" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install the Wally3k adlists.list?" 10 80) 
 then
 echo "User Declined Wally3k's adlists.list"
 else
