c76d41c357c56e1b72353976dfcfb30a2a08feae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:14:20 -0400

    Update eth0.sh

--- modules/eth0.sh
+++ modules/eth0.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## eth0 settings
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want set a static ip for eth0?" 8 78) then
 echo "User Declined Static IP for eth0"
 else
