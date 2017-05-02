ecee4aefc685b3ebd13d7964754c8d7bcce09e58
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:19:33 -0400

    Update raspcontrol.sh

--- modules/raspcontrol.sh
+++ modules/raspcontrol.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## raspcontrol
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install raspcontrol?" 8 78) then
 
 
