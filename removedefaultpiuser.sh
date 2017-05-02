ae3338da282b9b8ce1928a3a1eb51a272211595e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 15:34:12 -0400

    Update removedefaultpiuser.sh

--- removedefaultpiuser.sh
+++ removedefaultpiuser.sh
@@ -8,18 +8,15 @@ source /etc/piadvanced/install/userchange.conf
 
 ## Ask if Root, If Not exits
 { if 
-(whiptail --title "Remove User pi" --yes-button "I am Not Root" --no-button "I am Root" --yesno "Are you running as root?" 8 78)
+(whiptail --title "Remove User pi" --yes-button "I am Not Root" --no-button "I am Root" --yesno "Are you running as root?" 10 80)
 then
 echo "User Not Root"
 echo "Exiting"
 exit
 else
-whiptail -msgbox --title "Information" "
-Let's rename the user pi.
-This will make the pi a great deal more secure.
-I will ask you for a username and a password.
-" 20 70 1
-NEW_USERNAME=$(whiptail --title "New Username" --inputbox "Please enter desired username, you will then be prompted to create a new password" 20 60 "" 3>&1 1>&2 2>&3)
+echo "Let's rename the user pi. This will make the pi a great deal more secure. I will ask you for a username and a password." > changepi_textbox
+whiptail --textbox --title "Information" changepi_textbox 10 80
+NEW_USERNAME=$(whiptail --title "New Username" --inputbox "Please enter desired username, you will then be prompted to create a new password" 10 80 "" 3>&1 1>&2 2>&3)
 usermod -l $NEW_USERNAME -m -d /home/$NEW__USERNAME pi
 groupmod -n $NEW_USERNAME pi
 sudo passwd $NEW_USERNAME
