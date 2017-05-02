22e6e9faea145818897069927d2754b0045e53cc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:15:52 -0400

    Update hostname.sh

--- modules/hostname.sh
+++ modules/hostname.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## hostname settings
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want set the hostname?" 8 78) then
 echo "User Declined setting a new hostname"
 else
