178557fa434eb410d331de81b7b9d66ddd9ea7a9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:25:45 -0400

    Update ntp.sh

--- modules/ntp.sh
+++ modules/ntp.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
-## Timezone
+## NTP
 
 ## Variables
 source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the timezone?" 8 78) then
-echo "User Declined Setting timezone"
-else
-sudo dpkg-reconfigure tzdata
-fi }
-
-## NTP
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set ntp servers? serverlist is available at http://support.ntp.org/bin/view/Servers/NTPPoolServers" 8 78) then
 echo "User Declined Changing NTP Server"
 else
@@ -25,15 +18,4 @@ sudo sed -i "s/pool 3.debian.pool.ntp.org iburst/pool 3.$NEW_NTP iburst/" /etc/n
 sudo service ntp stop
 sudo ntpd -gq
 sudo service ntp start
-fi }
-
-## NTP Script
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set a script to refresh time?" 8 78) then
-echo "User Declined NTP Refresh Script"
-else
-sudo echo "sudo service ntp stop" | sudo tee --append /etc/piadvanced/installscripts/ntpupdate.sh
-sudo echo "sudo ntpd -gq
-sudo service n" | sudo tee --append /etc/piadvanced/installscripts/ntpupdate.sh
-sudo echo "sudo service ntp start" | sudo tee --append /etc/piadvanced/installscripts/ntpupdate.sh
-(crontab -l ; echo "0,30 * * * * sudo bash /etc/piadvanced/installscripts/ntpupdate.sh") | crontab -
 fi }
