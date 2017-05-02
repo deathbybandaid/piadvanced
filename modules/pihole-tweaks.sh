198ca234919c4b1c92a15bbbaff6051ca84e6cc6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 13:00:06 -0400

    Update pihole-tweaks.sh

--- modules/pihole-tweaks.sh
+++ modules/pihole-tweaks.sh
@@ -9,16 +9,16 @@ source /etc/piadvanced/install/userchange.conf
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install any Pi-Hole tweaks?" 8 78) then
 echo "User Declined Dark Pi-Hole Tweaks"
 else
-sudo bash /etc/piadvanced/modules/pihole-wally3kadlists.sh
-sudo bash /etc/piadvanced/modules/pihole-lkd70-darktheme.sh
-sudo bash /etc/piadvanced/modules/pihole-wally3kblockpage.sh
-sudo bash /etc/piadvanced/modules/pihole-deathbybandaid.sh
-sudo bash /etc/piadvanced/modules/pihole-autoupdate.sh
-sudo bash /etc/piadvanced/modules/pihole-gravity.sh
-sudo bash /etc/piadvanced/modules/pihole-ublock.sh
-sudo bash /etc/piadvanced/modules/pihole-youtubeadblock.sh
-sudo bash /etc/piadvanced/modules/pihole-adguard.sh
-sudo bash /etc/piadvanced/modules/pihole-phpparser.sh
-sudo bash /etc/piadvanced/modules/pihole-henningvanraumleyoutube.sh
-sudo bash /etc/piadvanced/modules/pihole-tweeter.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-wally3kadlists.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-lkd70-darktheme.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-wally3kblockpage.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-deathbybandaid.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-autoupdate.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-gravity.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-ublock.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-youtubeadblock.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-adguard.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-phpparser.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-henningvanraumleyoutube.sh
+sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-tweeter.sh
 fi }
