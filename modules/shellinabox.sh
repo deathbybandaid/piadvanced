7e532fce6b3207d719126063584cad24bb0dc858
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:20:01 -0400

    Update shellinabox.sh

--- modules/shellinabox.sh
+++ modules/shellinabox.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## shellinabox
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install shellinabox?" 8 78) then
 
 
