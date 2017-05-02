ee131e1a828ea4b9d859dca7818923941778e4a2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:15:43 -0400

    Update hass.sh

--- modules/hass.sh
+++ modules/hass.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 ## HASS
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install HASS? This install takes some decent time." 8 78) then
 echo "User Declined HASS"
 else
-curl -O https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/hass_rpi_installer.sh && sudo chown pi:pi hass_rpi_installer.sh && bash hass_rpi_installer.sh
-#sudo wget https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/hass_rpi_installer.sh -P  /etc/piadvanced/installscripts/
-#{ if [ "$CHANGED_USERNAME" = "yes" ]
-#then
-#sudo chown $NEW_USERNAME:$NEW_USERNAME /etc/piadvanced/installscripts/hass_rpi_installer.sh
-#else
-#sudo chown pi:pi /etc/piadvanced/installscripts/hass_rpi_installer.sh
-#fi }
-#bash /etc/piadvanced/installscripts/hass_rpi_installer.sh
-sudo mosquitto_passwd /etc/mosquitto/pwfile pi
-sudo systemctl restart mosquitto.service
+sudo wget https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/hass_rpi_installer.sh -P  /etc/piadvanced/installscripts/
+{ if [ "$CHANGED_USERNAME" = "yes" ]
+then
+sudo chown $NEW_USERNAME:$NEW_USERNAME /etc/piadvanced/installscripts/hass_rpi_installer.sh
+else
+sudo chown pi:pi /etc/piadvanced/installscripts/hass_rpi_installer.sh
+fi }
+bash /etc/piadvanced/installscripts/hass_rpi_installer.sh
 sudo echo "hassfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
