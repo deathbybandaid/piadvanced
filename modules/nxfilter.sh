d86765b8dabec204e0dbefbfa5383a40daf7cfdb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:24:02 -0400

    Update nxfilter.sh

--- modules/nxfilter.sh
+++ modules/nxfilter.sh
@@ -6,7 +6,7 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install nxfilter?" 8 78) then
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install nxfilter?" 10 80) then
 
 
 
