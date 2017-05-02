9539cd5ef5f995967aaf4465d13ecd0319bda1c1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:18:56 -0400

    Update plexboard.sh

--- modules/plexboard.sh
+++ modules/plexboard.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Plexboard
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Plexboard?" 8 78) then
 echo "User Declined Plexboard"
 else
