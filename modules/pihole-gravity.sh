d550d9067b9c6d76d6e7dd496e49caade1f82f09
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:40:13 -0400

    Create pihole-gravity.sh

---
+++ modules/pihole-gravity.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+## pihole
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
+{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a script to update gravity every 6 hours?" 8 78) then
+echo "User Declined Updating gravity every 6 hours"
+else
+(crontab -l ; echo "0 */6 * * * sudo bash /etc/piadvanced/piholetweaks/piholegravity.sh") | crontab -
+fi }
