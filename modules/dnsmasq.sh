efb39a0cd79126ec8d3f7259c42b1686e7a6e5b6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:13:26 -0400

    Update dnsmasq.sh

--- modules/dnsmasq.sh
+++ modules/dnsmasq.sh
@@ -6,10 +6,14 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using dnsmasq?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using dnsmasq?" 10 80) 
+then
 echo "User Declined DNSmasq"
 else
-{ if (whiptail --yes-button "Regular" --no-button "Experimental" --yesno "Do you want to use the experimental version 2.77test4?" 8 78) then
+{ if 
+(whiptail --yes-button "Regular" --no-button "Experimental" --yesno "Do you want to use the experimental version 2.77test4?" 10 80) 
+then
 sudo apt-get install -y dnsmasq
 else
 sudo bash /etc/piadvanced/installscripts/dnsmasqupgrade.sh
