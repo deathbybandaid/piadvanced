d072e19c34bc8fa766c421eac0d050343c03086b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:37:39 -0400

    Update xrdp.sh

--- modules/xrdp.sh
+++ modules/xrdp.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install xrdp?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install xrdp?" 10 80) 
+then
 echo "User Declined xrdp"
 else
 sudo apt-get install -y xrdp
