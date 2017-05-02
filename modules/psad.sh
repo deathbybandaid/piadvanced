def953bf980b160339d95553af5277e345e85244
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:19:16 -0400

    Update psad.sh

--- modules/psad.sh
+++ modules/psad.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## PSAD
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install psad?" 8 78) then
 echo "User Declined PSAD"
 else
