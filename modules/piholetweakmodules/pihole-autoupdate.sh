b0cdc3f50dbfb314083dba5e8a56a45f6b034f7c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 17:59:43 -0400

    Update pihole-autoupdate.sh

--- modules/piholetweakmodules/pihole-autoupdate.sh
+++ modules/piholetweakmodules/pihole-autoupdate.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do youw want to run pihole -up every 30 minutes?" 10 80) 
+(whiptail --title "Auto Update" --yes-button "Skip" --no-button "Proceed" --yesno "Do youw want to run pihole -up every 30 minutes?" 10 80) 
 then
 echo "User Declined 30 minute autoupdates"
 else
