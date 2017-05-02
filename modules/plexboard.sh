67c1c55ca1352c2ef052dce2dca7a5a6378f169c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:27:32 -0400

    Update plexboard.sh

--- modules/plexboard.sh
+++ modules/plexboard.sh
@@ -6,10 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Plexboard?" 8 78) then
-echo "User Declined Plexboard"
-else
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Warning: this takes 15 minutes on a pi3, 2 hours on the pi1" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Plexboard?" 10 80) 
+then
 echo "User Declined Plexboard"
 else
 curl -sSL https://get.rvm.io | bash -s stable
@@ -20,5 +19,4 @@ git clone https://github.com/scytherswings/Plex-Board.git && cd /opt/Plex-Board
 rvm install 2.3.4 && rvm use 2.3.4@plexdashboard --create
 ./serverSetup.sh
 sudo echo "plexboardfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
-fi }
 fi }
