60bbace91724799806d20f630b14f01fbdd42b15
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 15:53:04 -0400

    Update updateupgrade.sh

--- modules/updateupgrade.sh
+++ modules/updateupgrade.sh
@@ -6,10 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{whiptail --msgbox "I'm going to run updates." 20 70 1
+whiptail --msgbox "I'm going to run updates." 20 70 1
 sudo apt-get -y install
 sudo apt-get -y update --fix-missing
 sudo apt-get -y dist-upgrade -alow-downgrades
 sudo apt-get -y autoremove
 sudo apt-get clean
- }
