e05bd1ad7eca161e93a479a147f9c672b358a817
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 16:18:07 -0400

    Update pihole-adguard.sh

--- modules/piholetweakmodules/pihole-adguard.sh
+++ modules/piholetweakmodules/pihole-adguard.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to use a script to add adguard blocking?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to use a script to add adguard blocking?" 10 80) 
+then
 echo "User Declined adguard parser"
 else
 (crontab -l ; echo "0 3 * * * sudo bash /etc/piadvanced/piholetweaks/adguard.sh") | crontab -
