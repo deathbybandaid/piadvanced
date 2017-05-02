cb288b6bc018000fd5f12e84302edc6bf03c3161
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:16:07 -0400

    Update letsencrypt.sh

--- modules/letsencrypt.sh
+++ modules/letsencrypt.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Let'sencrypt
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 #sudo git clone https://github.com/certbot/certbot /etc/letsencrypt
 #sudo /etc/letsencrypt/certbot-auto certonly --agree-tos --webroot -w /data/mysite.com/www -d mysite.com -d www.mysite.com -d mail.mysite.com -d srv01.mysite.com
 
