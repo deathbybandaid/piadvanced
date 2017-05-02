3c399f4ae11e09685c094e5e7525d4dfe511fb1a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:21:27 -0400

    Update usermin.sh

--- modules/usermin.sh
+++ modules/usermin.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Usermin
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Usermin?" 8 78) then
 echo "User Declined Usermin"
 else
