a64cd9c120337ab31eeec38e5ccb39f50ce5a0ad
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:16:18 -0400

    Update lighttpd.sh

--- modules/lighttpd.sh
+++ modules/lighttpd.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Lighttpd
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on running Lighttpd" 8 78) then
 echo "User Decline Lighttpd"
 else
