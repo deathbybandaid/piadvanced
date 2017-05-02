fbe6958e6783ca08fcc5eb772c4e56d863e9a90f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:13:54 -0400

    Update dplatform.sh

--- modules/dplatform.sh
+++ modules/dplatform.sh
@@ -6,10 +6,12 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the Dplatform installer?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the Dplatform installer?" 10 80) 
+then
 echo "User Declined Dplatform"
 else
 sudo wget https://raw.githubusercontent.com/DFabric/DPlatform-ShellCore/master/init.sh -P /etc/piadvanced/installscripts/dplatform/
 sudo bash /etc/piadvanced/installscripts/dplatform/init.sh
-whiptail --msgbox "Any programs installed via Dplatform need firewall rules." 20 70 1
+whiptail --msgbox "Any programs installed via Dplatform need firewall rules." 10 80 1
 fi }
