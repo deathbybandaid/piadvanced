d46b6c0d1ca6903112d41e4045f6269fbd80328e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:01:25 -0400

    Update pihole-phpparser.sh

--- modules/piholetweakmodules/pihole-phpparser.sh
+++ modules/piholetweakmodules/pihole-phpparser.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to use the PHP Parser?" 10 80) 
+(whiptail --title "PHP Parser" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to use the PHP Parser?" 10 80) 
 then
 echo "User Declined the PHP Parser"
 else
