89c4632d8c39d7d36e9e85898e6c3dfc728e1b37
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:17:46 -0400

    Update ntp.sh

--- modules/ntp.sh
+++ modules/ntp.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Timezone
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the timezone?" 8 78) then
 echo "User Declined Setting timezone"
 else
