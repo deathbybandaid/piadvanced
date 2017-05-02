bea1d68b486858d02a7603a5c31949ca1c4528d9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 19:44:06 -0400

    Create hassinstall.sh

---
+++ installscripts/hassinstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+## HASS
+
+me=$(whoami)
+sudo apt-get --force-yes --yes install python-dev
+sudo apt-get --force-yes --yes install python-pip
+sudo apt-get --force-yes --yes install git
+sudo apt-get --force-yes --yes install libssl-dev
+sudo apt-get --force-yes --yes install libffi-dev
+sudo apt-get --force-yes --yes remove apt-listchanges
+sudo pip install --upgrade pip
+sudo pip install --upgrade setuptools
+sudo pip install pycrypto
+sudo pip install cryptography
+sudo pip install packaging
+sudo pip install appdirs
+sudo pip install six
+sudo pip install fabric
+git clone https://github.com/home-assistant/fabric-home-assistant.git /etc/piadvanced/fabric-home-assistant/
+cd /etc/piadvanced/fabric-home-assistant
+fab deploy_novenv -H localhost 2>&1 | sudo tee installation_report.txt
