10c4e138a8250726905478358f7f8b2c45458f34
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:19:53 -0400

    Update sambashare.sh

--- modules/sambashare.sh
+++ modules/sambashare.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ##Samba Share
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a samba share?" 8 78) then
 
 
