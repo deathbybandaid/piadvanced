4525a56c77bab9cbd2296437cc4bb5efdcfe807d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:54:44 -0400

    Update

--- modules/updateupgrade.sh
+++ modules/updateupgrade.sh
@@ -6,8 +6,13 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-whiptail --msgbox "I'm going to run updates." 10 80 1
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to run updates?" 10 80) 
+then
+echo "User Declined Updating"
+else
 sudo apt-get -y update --fix-missing
 sudo apt-get -y dist-upgrade -alow-downgrades
 sudo apt-get autoremove-y 
 sudo apt-get clean
+fi }
