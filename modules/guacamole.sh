823a40798b0d477b4d70a35ec20828a2a74e109f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:15:38 -0400

    Update guacamole.sh

--- modules/guacamole.sh
+++ modules/guacamole.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Guacamole?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Guacamole?" 10 80) 
+then
 echo "User Declined Guacamole"
 else
 sudo dos2unix /etc/piadvanced/installscripts/guacamole.sh
