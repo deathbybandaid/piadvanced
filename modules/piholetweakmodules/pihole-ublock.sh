043c33940fc4580a530855ed6e551da984608c31
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 16:21:41 -0400

    Update pihole-ublock.sh

--- modules/piholetweakmodules/pihole-ublock.sh
+++ modules/piholetweakmodules/pihole-ublock.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a program to parse additional lists? By default this uses lists used by ublock. To change the lists, edit lists.lst in /etc/piadvanced/installscripts/ublockpihole/" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want the Ublock Parser?" 10 80) 
+then
 echo "User Declined the UBlock Parser"
 else
 sudo cp -n /etc/piadvanced/piholetweaks/ublockpihole/lists.lst.default /etc/piadvanced/piholetweaks/ublockpihole/lists.lst
