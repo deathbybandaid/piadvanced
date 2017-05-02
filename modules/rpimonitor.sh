b22b42b71b3f00c6b287f0ea179126ee0b4bca62
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:30:35 -0400

    Update rpimonitor.sh

--- modules/rpimonitor.sh
+++ modules/rpimonitor.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install rpimonitor?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install rpimonitor?" 10 80) 
+then
 echo "User decline rpimonitor"
 else
 sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
