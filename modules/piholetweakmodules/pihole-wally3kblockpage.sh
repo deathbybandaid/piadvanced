13bf14e5e3bdfc7d83916a0608b8f8be5d08fb05
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 16:23:01 -0400

    Update pihole-wally3kblockpage.sh

--- modules/piholetweakmodules/pihole-wally3kblockpage.sh
+++ modules/piholetweakmodules/pihole-wally3kblockpage.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install the wally3k block page?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install the wally3k block page?" 10 80) 
+then
 echo "User Declined using Wally3k's Block Page"
 else
 sudo bash /etc/piadvanced/piholetweaks/Wally3kBlockPage.sh
