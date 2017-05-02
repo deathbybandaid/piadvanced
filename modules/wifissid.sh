be77dfc60373286ab9d2dd0ddc21d98c47951438
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:22:03 -0400

    Update wifissid.sh

--- modules/wifissid.sh
+++ modules/wifissid.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-##
+## wlan0 setup
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Connect to a wifi network using wlan0?" 8 78) then
 echo "User Declined Connecting to wifi"
 else
