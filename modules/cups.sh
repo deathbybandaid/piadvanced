106ec205686fbee161010cf8aba2e1bf2155b26a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:13:20 -0400

    Update cups.sh

--- modules/cups.sh
+++ modules/cups.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 ## Cups
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Cups?" 8 78) then
 echo "User Declined CUPS"
 else
