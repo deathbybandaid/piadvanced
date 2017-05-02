8fdb0bcb037a76715e6efdf354561acf37ab788a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:19:43 -0400

    Update rpimonitor.sh

--- modules/rpimonitor.sh
+++ modules/rpimonitor.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 #rpimonitor
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install rpimonitor?" 8 78) then
 echo "User decline rpimonitor"
 else
