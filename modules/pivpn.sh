c4a1ce52d85db580eb9c84a72425b27da769014b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:26:35 -0400

    Update pivpn.sh

--- modules/pivpn.sh
+++ modules/pivpn.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install pivpn?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install pivpn?" 10 80) 
+then
 echo "User Declined pivpn"
 else
 curl -L https://install.pivpn.io | bash
