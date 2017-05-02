6b9b4246ef76aea87acc22da1f46cf4a42baafe9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:36:56 -0400

    Update wifissid.sh

--- modules/wifissid.sh
+++ modules/wifissid.sh
@@ -6,11 +6,13 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Connect to a wifi network using wlan0?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Connect to a wifi network using wlan0?" 10 80) 
+then
 echo "User Declined Connecting to wifi"
 else
-NEW_SSID=$(whiptail --inputbox "Please enter SSID" 20 60 "" 3>&1 1>&2 2>&3)
-NEW_PSK=$(whiptail --inputbox "Please enter wifi password" 20 60 "" 3>&1 1>&2 2>&3)
+NEW_SSID=$(whiptail --inputbox "Please enter SSID" 10 80 "" 3>&1 1>&2 2>&3)
+NEW_PSK=$(whiptail --inputbox "Please enter wifi password" 10 80 "" 3>&1 1>&2 2>&3)
 sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/piadvanced/backups/
 sudo echo "NEWWLAN_IP=$NEWWLAN_IP" | sudo tee --append /etc/piadvanced/install/variables.conf
 sudo echo "NEW_SSID=$NEW_SSID" | sudo tee --append /etc/piadvanced/install/variables.conf
