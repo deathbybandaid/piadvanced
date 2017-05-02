f95cc689b40bd4464cffd63fca95c582450fcf74
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 12:21:46 -0400

    Update webmin.sh

--- modules/webmin.sh
+++ modules/webmin.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ## Webmin
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
 { if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Webmin?" 8 78) then
 echo "User Declined Webmin"
 else
