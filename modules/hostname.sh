58747e6fcd5ed4cc836a7f83a00cb90556a27aa6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 18:16:31 -0400

    Update hostname.sh

--- modules/hostname.sh
+++ modules/hostname.sh
@@ -6,7 +6,9 @@ source /etc/piadvanced/install/firewall.conf
 source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
-{ if (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want set the hostname?" 8 78) then
+{ if 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want set the hostname?" 10 80) 
+then
 echo "User Declined setting a new hostname"
 else
 OLD_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
