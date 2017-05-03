f1bf4742b31b6d2bfd46ae5ef114725097f7f3d4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:53:41 -0400

    Update pihole-stalelist.sh

--- modules/piholetweakmodules/pihole-stalelist.sh
+++ modules/piholetweakmodules/pihole-stalelist.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## pihole stalelist fix
+NAMEOFAPP="piholestalelistfix"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,9 +11,11 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Stale List Fix" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a script to keep cached lists from becoming stale?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a script to keep cached lists from becoming stale?" 10 80) 
 then
-echo "User Declined Stale List Fix"
+echo "User Declined $NAMEOFAPP"
 else
 (crontab -l ; echo "0 5 * * 1 sudo bash /etc/piadvanced/piholetweaks/piholefreshlists.sh") | crontab -
 fi }
+
+unset NAMEOFAPP
