359e3352c8a4fc0164e1964344cc3e168c6f86ff
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:41:28 -0400

    Update pihole-lkd70-darktheme.sh

--- modules/piholetweakmodules/pihole-lkd70-darktheme.sh
+++ modules/piholetweakmodules/pihole-lkd70-darktheme.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" -yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Dark webui theme?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo wget https://raw.githubusercontent.com/lkd70/PiHole-Dark/master/install.sh -P /var/www/html/
 cd /var/www/html
 sudo bash install.sh
