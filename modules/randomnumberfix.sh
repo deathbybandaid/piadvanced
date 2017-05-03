b6ef115697c6789f8fe7d8c7989738e5268c4d32
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:38:19 -0400

    Update randomnumberfix.sh

--- modules/randomnumberfix.sh
+++ modules/randomnumberfix.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## Random Number fix
+NAMEOFAPP="rngtools"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -12,9 +13,11 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "RNG-Tools" --yes-button "Skip" --no-button "Proceed" --yesno "Do you wnat to install the random number fix?" 10 80) 
 then
-echo "User Declined RNG"
+echo "User Declined $NAMEOFAPP"
 else
 sudo apt-get install -y rng-tools
 sudo cp /etc/default/rng-tools /etc/piadvanced/backups/
 sudo echo 'HRNGDEVICE=/dev/urandom' | sudo tee --append /etc/default/rng-tools
 fi }
+
+unset NAMEOFAPP
