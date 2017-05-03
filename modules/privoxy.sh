f065e0dc60ce63e33b974153dce2ac8aca29aa57
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:55:52 -0400

    Update privoxy.sh

--- modules/privoxy.sh
+++ modules/privoxy.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Privoxy?" 10 80) 
+(whiptail --title "Privoxy" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Privoxy?" 10 80) 
 then
 echo "User Declined Privoxy"
 else
