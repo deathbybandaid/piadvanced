fce237e18347ec3fd5034950c3342f6a6efd835a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:38:17 -0400

    Update totalmemtweak.sh

--- modules/totalmemtweak.sh
+++ modules/totalmemtweak.sh
@@ -14,7 +14,9 @@ source /etc/piadvanced/install/userchange.conf
 (whiptail --title "Ram Tweak" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to try the experimental unlock of an extra 16MB. This is for the Pi2 and Pi3 only" 10 80) 
 then
 sudo sed -i '/total_mem/ d' /boot/config.txt
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 NEWMEM_UNLOCK=$(whiptail --inputbox "Setting to 1024 is the Maximum. It may be prudent to say 1023" 10 80 "1023" 3>&1 1>&2 2>&3)
 sudo sed -i '/total_mem/ d' /boot/config.txt
 sudo echo "total_mem=$NEWMEM_UNLOCK" | sudo tee --append /boot/config.txt
