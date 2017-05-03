2e685c06f06651f2de6502b1265d3ed60d16fca8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:39:53 -0400

    Update webmin.sh

--- modules/webmin.sh
+++ modules/webmin.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
 sudo dpkg --install webmin_1.831_all.deb
