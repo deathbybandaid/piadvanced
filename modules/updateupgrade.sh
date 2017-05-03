2f21f47490892d0a867772f261c3868308ce565f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:39:25 -0400

    Update updateupgrade.sh

--- modules/updateupgrade.sh
+++ modules/updateupgrade.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to run updates?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo apt-get -y update --fix-missing
 sudo apt-get -y dist-upgrade -alow-downgrades
 sudo apt-get autoremove-y 
