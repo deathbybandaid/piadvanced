38c6a4ded04ed3527609df303010a8e2eb414b42
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:53:47 -0400

    Update pihole-tweaks.sh

--- modules/pihole-tweaks.sh
+++ modules/pihole-tweaks.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install any Pi-Hole tweaks?" 10 80) 
+(whiptail --title "Pi-Hole tweaks" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install any Pi-Hole tweaks?" 10 80) 
 then
 echo "User Declined Dark Pi-Hole Tweaks"
 else
