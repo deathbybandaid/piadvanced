7307e963525084a6b951e3b25047216f123f7d49
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:44:43 -0400

    Update dnscrypt.sh

--- modules/dnscrypt.sh
+++ modules/dnscrypt.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install DNSCrypt?" 10 80) 
+(whiptail --title "DNSCrypt" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install DNSCrypt?" 10 80) 
 then
 echo "User Declined DNSCrypt"
 else
