c4b3aca36aae27ce4ed23acb414c10d0f5f99aec
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:25:22 -0400

    Update EXAMPLEMODULE.sh

--- EXAMPLEMODULE.sh
+++ EXAMPLEMODULE.sh
@@ -13,8 +13,10 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 8 78) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+
 ## Need a message box?
 #whiptail --msgbox "$NAMEOFAPP messagebox" 20 70 1
 
