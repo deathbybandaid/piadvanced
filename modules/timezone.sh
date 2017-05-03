cce3aada2254985f00ad7b39e42e035269510005
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 10:01:01 -0400

    Update timezone.sh

--- modules/timezone.sh
+++ modules/timezone.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the timezone?" 10 80) 
+(whiptail --title "Timezone" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the timezone?" 10 80) 
 then
 echo "User Declined Setting timezone"
 else
