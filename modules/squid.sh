1564dd1d6857a4ee81128c07a0521db473bc89f0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 10:00:02 -0400

    Update squid.sh

--- modules/squid.sh
+++ modules/squid.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Squid and Squidguard?" 10 80) 
+(whiptail --title "Squid" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Squid and Squidguard?" 10 80) 
 then
 echo "User Declined Squid"
 else
