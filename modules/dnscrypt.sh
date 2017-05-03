68661b26b4e7b3fe7fca3047fe71c1562301ea92
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:16:40 -0400

    Update dnscrypt.sh

--- modules/dnscrypt.sh
+++ modules/dnscrypt.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## DNSCRYPT
+NAMEOFAPP="dnscrypt"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,11 +11,13 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "DNSCrypt" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install DNSCrypt?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined DNSCrypt"
+echo "User Declined $NAMEOFAPP"
 else
 sudo bash /etc/piadvanced/installscripts/dnsproxy/dnscryptinstall.sh
-sudo echo "dnscryptfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 sudo cp -n /etc/piadvanced/piholetweaks/dnscrypt/10-dnscrypt.conf /etc/dnsmasq.d/
 fi }
+
+unset NAMEOFAPP
