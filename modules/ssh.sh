cdc7cb48d55ef8425175b97fe7495b0ab044f8d1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:43:48 -0400

    Update ssh.sh

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## SSH
 ## I might add the option to use a key versus password for login.
+NAMEOFAPP="ssh"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -11,7 +12,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "SSH" --yes-button "Disable" --no-button "Enable" --yesno "Would you like the SSH server enabled or disabled?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Disable" --no-button "Enable" --yesno "Would you like the SSH server enabled or disabled?" 10 80) 
 then
 update-rc.d ssh disable &&
 whiptail --msgbox "SSH server disabled" 10 80 1
@@ -19,5 +20,7 @@ else
 update-rc.d ssh enable &&
 invoke-rc.d ssh start &&
 whiptail --msgbox "SSH server enabled" 10 80 1
-sudo echo "sshfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
+
+unset NAMEOFAPP
