c4e248c6a0143581609f44ccacbb0c7205714c6d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 10:02:20 -0400

    Update wakeonlan.sh

--- modules/wakeonlan.sh
+++ modules/wakeonlan.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install WOL?" 10 80) 
+(whiptail --title "Wake-On-Lan" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install WOL?" 10 80) 
 then
 
 
