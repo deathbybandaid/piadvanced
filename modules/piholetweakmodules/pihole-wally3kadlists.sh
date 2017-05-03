328833bf5198beb277b96451f40558f2edef3cc7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:55:10 -0400

    Update pihole-wally3kadlists.sh

--- modules/piholetweakmodules/pihole-wally3kadlists.sh
+++ modules/piholetweakmodules/pihole-wally3kadlists.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## Wally3kadlists
+NAMEOFAPP="wally3kadlists"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,9 +11,11 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Wally3k adlists.list" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install the Wally3k adlists.list?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined Wally3k's adlists.list"
+echo "User Declined $NAMEOFAPP"
 else
 sudo wget https://raw.githubusercontent.com/deathbybandaid/wally3k-adlists.list/master/adlists.list -P /etc/pihole/
 fi }
+
+unset NAMEOFAPP
