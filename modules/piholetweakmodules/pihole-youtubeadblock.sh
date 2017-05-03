47d6365a3b173889847198897aa6c157078a64d9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:56:23 -0400

    Update pihole-youtubeadblock.sh

--- modules/piholetweakmodules/pihole-youtubeadblock.sh
+++ modules/piholetweakmodules/pihole-youtubeadblock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## youtube adblocker
+NAMEOFAPP="youtubeadblocker"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,11 +11,13 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Youtube adlocking" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to use an script to add additional youtube adblocking?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined the youtube ad blocker"
+echo "User Declined $NAMEOFAPP"
 else
 sudo pip install -r /etc/piadvanced/piholetweaks/youtubeadblock/requirements.txt
 (crontab -l ; echo "0 2 * * * sudo bash /etc/piadvanced/piholetweaks/youtubeadblock/youtube-ads.sh") | crontab -
 sudo echo "#http://localhost/admin/youtube.txt" | sudo tee --append /etc/pihole/adlists.list
 fi }
+
+unset NAMEOFAPP
