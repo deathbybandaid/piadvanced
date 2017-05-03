f3c7a777e038d7d5881431501f6c1693ee793ef1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:42:01 -0400

    Update pihole-ublock.sh

--- modules/piholetweakmodules/pihole-ublock.sh
+++ modules/piholetweakmodules/pihole-ublock.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want the $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo cp -n /etc/piadvanced/piholetweaks/ublockpihole/lists.lst.default /etc/piadvanced/piholetweaks/ublockpihole/lists.lst
 (crontab -l ; echo "0 1 * * * sudo bash /etc/piadvanced/piholetweaks/ublockpihole/ublockpihole.sh") | crontab -
 sudo echo "#http://localhost/admin/ublock.txt" | sudo tee --append /etc/pihole/adlists.list
