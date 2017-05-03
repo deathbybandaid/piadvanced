dc55f317ec53c9fb2243ccb925db52f77b46c774
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 12:51:11 -0400

    Update EXAMPLEMODULE.sh

--- EXAMPLEMODULE.sh
+++ EXAMPLEMODULE.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## NAMEOFAPP
+NAMEOFAPP="PUTNAMEOFAPPHERE"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,26 +11,31 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install ?" 8 78) 
+(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 8 78) 
 then
-echo "User Declined NAMEOFAPP"
-
+echo "User Declined $NAMEOFAPP"
 else
 ## Need a message box?
-whiptail --msgbox "NAMEOFAPP messagebox" 20 70 1
+#whiptail --msgbox "$NAMEOFAPP messagebox" 20 70 1
 
 ## Set a variable with a text box?
-NEW_VARIABLE=$(whiptail --inputbox "NAMEOFAPP what do you want to type?" 20 60 "NAMEOFAPP suggested text" 3>&1 1>&2 2>&3)
+#NEW_VARIABLE=$(whiptail --inputbox "NAMEOFAPP what do you want to type?" 10 80 "$NAMEOFAPP suggested text" 3>&1 1>&2 2>&3)
 
 
-echo "installing stuff"
+echo "installing $NAMEOFAPP"
 
 ## Execute install script
-sudo bash /etc/piadvanced/installscripts/NAMEOFAPP.sh
+#sudo bash /etc/piadvanced/installscripts/NAMEOFAPPinstall.sh
 
 ## If Firewall rule is needed
-sudo echo "NAMEOFAPPfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo "$NAMEOFAPP-firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 
 ## Save variable to the conf
-sudo echo "NEW_VARIABLE=$NEW_VARIABLE" | sudo tee --append /etc/piadvanced/install/variables.conf
+#sudo echo "NEW_VARIABLE=$NEW_VARIABLE" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## End of install
 fi }
+
+unset NAMEOFAPP
+
+
