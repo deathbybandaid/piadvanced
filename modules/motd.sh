304c5c10dd031087524e954754edecacea58ad10
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:51:41 -0400

    Update motd.sh

--- modules/motd.sh
+++ modules/motd.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Change your message of the day to a much cooler one?" 10 80) 
+(whiptail --title "Message Of The Day" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Change your message of the day to a much cooler one?" 10 80) 
 then
 echo "User Declined setting a cooler motd message"
 else
