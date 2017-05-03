cb47caab5dd411d13815fedede6965a1aa74c42b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:29:28 -0400

    Update atomic.sh

--- modules/atomic.sh
+++ modules/atomic.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP ?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo git clone https://github.com/htpcBeginner/AtoMiC-ToolKit ~/AtoMiC-ToolKit
 sudo bash ~/AtoMiC-ToolKit/setup.sh
 whiptail --msgbox "Any programs installed via Atomic need firewall rules." 10 80 1
