5519c89a37f3b0e41e69a3f9efdf2bc9b74dfa7f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:46:34 -0400

    Update usermin.sh

--- modules/usermin.sh
+++ modules/usermin.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## Usermin
+NAMEOFAPP="usermin"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,12 +11,14 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Usermin" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Usermin?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined Usermin"
+echo "User Declined $NAMEOFAPP"
 else
 sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl
 sudo wget http://prdownloads.sourceforge.net/webadmin/usermin_1.701_all.deb
 sudo dpkg --install usermin_1.701_all.deb
-sudo echo "userminfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
+
+unset NAMEOFAPP
