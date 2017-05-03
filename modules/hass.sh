fa9027eddaa306c022ec34b041d0a3bd697a0e0e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:24:09 -0400

    Update hass.sh

--- modules/hass.sh
+++ modules/hass.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## HASS
+NAMEOFAPP="hass"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,9 +11,9 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Home Automation" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install HASS? This install takes some decent time." 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP ?" 10 80) 
 then
-echo "User Declined HASS"
+echo "User Declined $NAMEOFAPP"
 else
 #sudo wget https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/hass_rpi_installer.sh -P  /etc/piadvanced/installscripts/
 #{ if [ "$CHANGED_USERNAME" = "yes" ]
@@ -23,5 +24,7 @@ else
 #fi }
 #bash /etc/piadvanced/installscripts/hass_rpi_installer.sh
 sudo bash /etc/piadvanced/installscripts/hassinstall.sh
-sudo echo "hassfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
+
+unset NAMEOFAPP
