5265823445213e6fbfc4df333924ad2aa7f8c54a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-02 20:09:19 -0400

    Update hassinstall.sh

--- installscripts/hassinstall.sh
+++ installscripts/hassinstall.sh
@@ -25,6 +25,32 @@ sudo pip install packaging
 sudo pip install appdirs
 sudo pip install six
 sudo pip install fabric
+sudo apt-get install  -y build-essential
+sudo apt-get install  -y python-pip
+sudo apt-get install  -y python-dev
+sudo apt-get install  -y python3
+sudo apt-get install  -y python3-dev
+sudo apt-get install  -y python3-pip
+sudo apt-get install  -y python3-sphinx
+sudo apt-get install  -y python3-setuptools
+sudo apt-get install  -y git
+sudo apt-get install  -y libssl-dev
+sudo apt-get install  -y cmake
+sudo apt-get install  -y libc-ares-dev
+sudo apt-get install  -y uuid-dev
+sudo apt-get install  -y daemon
+sudo apt-get install  -y curl
+sudo apt-get install  -y libgnutls28-dev
+sudo apt-get install  -y libgnutlsxx28
+sudo apt-get install  -y libgnutls-dev
+sudo apt-get install  -y nmap
+sudo apt-get install  -y net-tools
+sudo apt-get install  -y sudo
+sudo apt-get install  -y libglib2.0-dev
+sudo apt-get install  -y cython3
+sudo apt-get install  -y libudev-dev
+sudo apt-get install  -y libxrandr-dev
+sudo apt-get install  -y swig
 sudo git clone https://github.com/home-assistant/fabric-home-assistant.git /etc/piadvanced/fabric-home-assistant/
 cd /etc/piadvanced/fabric-home-assistant
 fab deploy_novenv -H localhost 2>&1 | sudo tee installation_report.txt
