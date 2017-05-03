d833885d0ca0eb46441360037f6d23394c2525aa
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 10:01:31 -0400

    Update totalmemtweak.sh

--- modules/totalmemtweak.sh
+++ modules/totalmemtweak.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to try the experimental unlock of an extra 16MB. This is for the Pi2 and Pi3 only" 10 80) 
+(whiptail --title "Ram Tweak" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to try the experimental unlock of an extra 16MB. This is for the Pi2 and Pi3 only" 10 80) 
 then
 sudo sed -i '/total_mem/ d' /boot/config.txt
 else
