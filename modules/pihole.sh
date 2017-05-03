13b49ccd5629bbf3be6758caafa47c1e7ed095c6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:35:35 -0400

    Update pihole.sh

--- modules/pihole.sh
+++ modules/pihole.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## pihole
+NAMEOFAPP="pihole"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,9 +11,9 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Pi-Hole" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install pihole?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined Pi-Hole"
+echo "User Declined $NAMEOFAPP"
 else
 git clone --depth 1 https://github.com/pi-hole/pi-hole.git /etc/piadvanced/installscripts/Pi-hole
 cd /etc/piadvanced/installscripts/Pi-hole/automated\ install/
@@ -20,6 +21,8 @@ bash basic-install.sh
 whiptail --msgbox "Let's change pihole password" 10 80 1
 NEW_PASS=$(whiptail --inputbox "Please enter a password" 10 80 "" 3>&1 1>&2 2>&3)
 pihole -a -p $NEW_PASS
-sudo echo "piholefirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 sudo bash /etc/piadvanced/modules/pihole-tweaks.sh
 fi }
+
+unset NAMEOFAPP
