f864faf3ec299c1afbc4520b63bcbe4bfb1f86ac
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:57:41 -0400

    Update FIREWALL.sh

--- FIREWALL.sh
+++ FIREWALL.sh
@@ -321,28 +321,28 @@ sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
 fi }
 
 ## Privoxy
-#{ if 
-#[ "$privoxyfirewall" = "yes" ]
-#then
-#sudo echo "## Privoxy" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "-A PREROUTING -i eth0 –p tcp -–dport 80 -j REDIRECT --to-port 8118" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "-A PREROUTING -i wlan0 –p tcp -–dport 80 -j REDIRECT --to-port 8118" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "-A PREROUTING -i tun0 –p tcp -–dport 80 -j REDIRECT --to-port 8118" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
-#fi }
+{ if 
+[ "$privoxyfirewall" = "yes" ]
+then
+sudo echo "## Privoxy" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "#-A PREROUTING -i eth0 –p tcp -–dport 80 -j REDIRECT --to-port 8118" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "#-A PREROUTING -i wlan0 –p tcp -–dport 80 -j REDIRECT --to-port 8118" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "#-A PREROUTING -i tun0 –p tcp -–dport 80 -j REDIRECT --to-port 8118" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+fi }
 
 ## Squid
-#{ if 
-#[ "$squidfirewall" = "yes" ]
-#then
-#sudo echo "## Squid" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "-A PREROUTING -i eth0 –p tcp -–dport 80 -j REDIRECT --to-port 3128" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "-A PREROUTING -i wlan0 –p tcp -–dport 80 -j REDIRECT --to-port 3128" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "-A PREROUTING -i tun0 –p tcp -–dport 80 -j REDIRECT --to-port 3128" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
-#sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
-#fi }
+{ if 
+[ "$squidfirewall" = "yes" ]
+then
+sudo echo "## Squid" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "#-A PREROUTING -i eth0 –p tcp -–dport 80 -j REDIRECT --to-port 3128" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "#-A PREROUTING -i wlan0 –p tcp -–dport 80 -j REDIRECT --to-port 3128" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "#-A PREROUTING -i tun0 –p tcp -–dport 80 -j REDIRECT --to-port 3128" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+sudo echo "" | sudo tee --append /etc/iptables.firewall.rules
+fi }
 
 ## End of *nat
 sudo echo "COMMIT" | sudo tee --append /etc/iptables.firewall.rules
@@ -359,6 +359,8 @@ sudo cp /etc/piadvanced/installscripts/firewall.sh /etc/network/if-pre-up.d/fire
 else
 echo "User Declined Firewall"
 fi }
+
+
 ## sudo iptables-restore < /etc/iptables.firewall.rules
 
 ## End of file
