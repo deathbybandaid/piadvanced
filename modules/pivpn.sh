89b53167dba9691acfcd805613cbfff57fbc45dd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:55:08 -0400

    Update pivpn.sh

--- modules/pivpn.sh
+++ modules/pivpn.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install pivpn?" 10 80) 
+(whiptail --title "pivpn" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install pivpn?" 10 80) 
 then
 echo "User Declined pivpn"
 else
