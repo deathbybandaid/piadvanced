f1e6fd4723c1831c19f046b28e41cb4752ad69c5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 19:50:59 -0400

    Create htop.sh

---
+++ modules/htop.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+## htop
+NAMEOFAPP="htop"
+
+## Dependencies Check
+sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
+{ if 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP ?" 10 80) 
+then
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+sudo apt-get install -y htop
+fi }
+
+unset NAMEOFAPP
