854cbdb3ef36fc678f4e481d1fd18916a1f12416
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:35:11 -0400

    Update psad.sh

--- modules/psad.sh
+++ modules/psad.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo wget http://cipherdyne.org/psad/download/psad-2.4.4.tar.gz -P /etc/piadvanced/installscripts/
 sudo tar xzf psad-2.4.4.tar.gz
 cd psad-2.4.4
