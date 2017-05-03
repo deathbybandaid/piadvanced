fb02e9e5ebfa24c16125bd940ce6b61d3ebbc427
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:40:40 -0400

    Update pihole-autoupdate.sh

--- modules/piholetweakmodules/pihole-autoupdate.sh
+++ modules/piholetweakmodules/pihole-autoupdate.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do youw want to run pihole -up every 30 minutes?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 (crontab -l ; echo "0,30 * * * * sudo bash /etc/piadvanced/piholetweaks/piholeautoupdate.sh") | crontab -
 fi }
 
