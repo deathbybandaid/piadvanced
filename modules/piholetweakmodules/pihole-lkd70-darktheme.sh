d66ed7a9a56faae5494aecfd437075993e4764ba
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:52:55 -0400

    Update pihole-lkd70-darktheme.sh

--- modules/piholetweakmodules/pihole-lkd70-darktheme.sh
+++ modules/piholetweakmodules/pihole-lkd70-darktheme.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## Dark Pi-Hole Theme
+NAMEOFAPP="piholedarktheme"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,11 +11,13 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Dark webui" -yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Dark webui theme?" 10 80) 
+(whiptail --title "$NAMEOFAPP" -yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Dark webui theme?" 10 80) 
 then
-echo "User Declined Dark Theme"
+echo "User Declined $NAMEOFAPP"
 else
 sudo wget https://raw.githubusercontent.com/lkd70/PiHole-Dark/master/install.sh -P /var/www/html/
 cd /var/www/html
 sudo bash install.sh
 fi }
+
+unset NAMEOFAPP
