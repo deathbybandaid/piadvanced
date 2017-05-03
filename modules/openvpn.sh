0e3322cb71cd1e9aa898364e9eab0c4510ee67b0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:53:28 -0400

    Update openvpn.sh

--- modules/openvpn.sh
+++ modules/openvpn.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using the OpenVPN Server?" 10 80) 
+(whiptail --title "OpenVPN" --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using the OpenVPN Server?" 10 80) 
 then
 echo "User Declined OpenVPN"
 else
