30a39902407f692cb4b10f84d19624376b11d6ea
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:40:47 -0400

    Update pihole-deathbybandaid.sh

--- modules/piholetweakmodules/pihole-deathbybandaid.sh
+++ modules/piholetweakmodules/pihole-deathbybandaid.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 8 78) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo cp -n /etc/piadvanced/piholetweaks/dnsmasqtweaks/*.conf /etc/dnsmasq.d/
 fi }
 
