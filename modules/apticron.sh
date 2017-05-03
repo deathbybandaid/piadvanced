fb6091e25028c2fa8a30a115f4b91fcbc8cb46c7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:29:16 -0400

    Update apticron.sh

--- modules/apticron.sh
+++ modules/apticron.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP ?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 APTICRON_EMAIL=$(whiptail --inputbox "What email address do you want to use?" 10 80 "user@domain.net" 3>&1 1>&2 2>&3)
 sudo apt-get install apticron
 sudo sed -i "s/EMAIL="root"/EMAIL="$APTICRON_EMAIL"/" /etc/apticron/apticron.conf
