13f102163a35ee801f6076533bf9f9c37e9f4128
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:39:39 -0400

    Update usermin.sh

--- modules/usermin.sh
+++ modules/usermin.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl
 sudo wget http://prdownloads.sourceforge.net/webadmin/usermin_1.701_all.deb
 sudo dpkg --install usermin_1.701_all.deb
