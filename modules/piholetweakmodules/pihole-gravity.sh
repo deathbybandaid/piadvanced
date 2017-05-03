a791970d473b68eb4e13341694ae8f4b91a9513d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:52:03 -0400

    Update pihole-gravity.sh

--- modules/piholetweakmodules/pihole-gravity.sh
+++ modules/piholetweakmodules/pihole-gravity.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## pihole gravity
+NAMEOFAPP="piholegravity"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,9 +11,11 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Gravity" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a script to update gravity every 6 hours?" 8 78) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want a script to update gravity every 6 hours?" 8 78) 
 then
-echo "User Declined Updating gravity every 6 hours"
+echo "User Declined $NAMEOFAPP"
 else
 (crontab -l ; echo "0 */6 * * * sudo bash /etc/piadvanced/piholetweaks/piholegravity.sh") | crontab -
 fi }
+
+unset NAMEOFAPP
