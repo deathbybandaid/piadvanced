62d4319ea9455cf9580831e3857c0e6ffb429619
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:46:12 -0400

    Update exim4.sh

--- modules/exim4.sh
+++ modules/exim4.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install exim4?" 10 80) 
+(whiptail --title "Exim4" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install exim4?" 10 80) 
 then
 echo "User Declined Exim4"
 else
