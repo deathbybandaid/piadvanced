f8b7de6ae44ff323dc846a904cc7e74ed7c35b16
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:43:18 -0400

    Update atomic.sh

--- modules/atomic.sh
+++ modules/atomic.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the AtoMiC-ToolKit for htpc softwares?" 10 80) 
+(whiptail --title "AtoMiC-ToolKit" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the AtoMiC-ToolKit for htpc softwares?" 10 80) 
 then
 echo "User Declined Atomic"
 else
