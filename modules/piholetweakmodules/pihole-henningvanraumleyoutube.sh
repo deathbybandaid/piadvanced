9cefeaac14c13cd3f63ab376dd8c8f08383539cf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:41:21 -0400

    Update pihole-henningvanraumleyoutube.sh

--- modules/piholetweakmodules/pihole-henningvanraumleyoutube.sh
+++ modules/piholetweakmodules/pihole-henningvanraumleyoutube.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "Youtube adblock list" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to use HenningVanRaumle's youtube adblock list?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo echo "#https://raw.githubusercontent.com/HenningVanRaumle/pihole-ytadblock/master/ytadblock.txt" | sudo tee --append /etc/pihole/adlists.list
 fi }
 
