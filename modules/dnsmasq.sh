e664240942fb5f0719d3024632d55d7b07d96d86
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:19:14 -0400

    Update dnsmasq.sh

--- modules/dnsmasq.sh
+++ modules/dnsmasq.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## DNSMasq
+NAMEOFAPP="dnsmasq"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,15 +11,17 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "DNSmasq" --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using dnsmasq?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using $NAMEOFAPP ?" 10 80) 
 then
-echo "User Declined DNSmasq"
+echo "User Declined $NAMEOFAPP"
 else
 { if 
-(whiptail --yes-button "Regular" --no-button "Experimental" --yesno "Do you want to use the experimental version 2.77test4?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Regular" --no-button "Experimental" --yesno "Do you want to use the experimental version 2.77test4?" 10 80) 
 then
 sudo apt-get install -y dnsmasq
 else
 sudo bash /etc/piadvanced/installscripts/dnsmasqupgrade.sh
 fi }
 fi }
+
+unset NAMEOFAPP
