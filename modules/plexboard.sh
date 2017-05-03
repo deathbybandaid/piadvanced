b73223d6ef95e64a017f746804bcef3c183d35e8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:34:21 -0400

    Update plexboard.sh

--- modules/plexboard.sh
+++ modules/plexboard.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/pi/.rvm/scripts/rvm
 rvm requirements
