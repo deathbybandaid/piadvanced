a5b064637dafe307f78708daaf9bba4d0871679f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:33:49 -0400

    Update openvpn.sh

--- modules/openvpn.sh
+++ modules/openvpn.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## OpenVPN
+NAMEOFAPP="openvpn"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,14 +11,16 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "OpenVPN" --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using the OpenVPN Server?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using $NAMEOFAPP ?" 10 80) 
 then
-echo "User Declined OpenVPN"
+echo "User Declined $NAMEOFAPP"
 else
 whiptail --msgbox "Select TCP during the install" 10 80 1
 sudo wget https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -P /etc/piadvanced/installscripts/
 sudo chmod +x /etc/piadvanced/installscripts/openvpn-install.sh
 sudo bash /etc/piadvanced/installscripts/openvpn-install.sh
-sudo echo "openvpnfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 sudo sed -i "s/push "dhcp-option DNS 8.8.8.8"/"dhcp-option DNS 10.8.0.1"/" /etc/openvpn/server.conf
 fi }
+
+unset NAMEOFAPP
