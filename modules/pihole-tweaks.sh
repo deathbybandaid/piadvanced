d22762b308d0054dfaeee5739bc8d08701b939fc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:33:57 -0400

    Update pihole-tweaks.sh

--- modules/pihole-tweaks.sh
+++ modules/pihole-tweaks.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "Pi-Hole tweaks" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install any Pi-Hole tweaks?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-wally3kadlists.sh
 sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-lkd70-darktheme.sh
 sudo bash /etc/piadvanced/modules/piholetweakmodules/pihole-wally3kblockpage.sh
