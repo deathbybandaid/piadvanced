3623471ad63b9cffcd1ce10d614574f35c186ed3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:33:27 -0400

    Update ntp.sh

--- modules/ntp.sh
+++ modules/ntp.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "NTP Server" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set ntp servers? serverlist is available at http://support.ntp.org/bin/view/Servers/NTPPoolServers" 10 80) 
 then
-echo "User Declined Changing NTP Server"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 NEW_NTP=$(whiptail --inputbox "Set server here" 10 80 "debian.pool.ntp.org" 3>&1 1>&2 2>&3)
 sudo cp /etc/ntp.conf /etc/piadvanced/backups/
 sudo sed -i "s/pool 0.debian.pool.ntp.org iburst/pool 0.$NEW_NTP iburst/" /etc/ntp.conf
