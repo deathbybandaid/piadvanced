1c720256ae1f5d0add71e26742ad6b7d18f4a193
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 08:12:49 -0400

    Update ublockpihole.sh

--- piholetweaks/ublockpihole/ublockpihole.sh
+++ piholetweaks/ublockpihole/ublockpihole.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 sudo rm /var/www/html/admin/ublock.txt
-for source in `cat lists.lst`; do
+for source in `cat /etc/piadvanced/piholetweaks/ublockpihole/lists.lst`; do
     echo $source;
-    sudo curl --silent $source >> ads.txt
+    sudo curl --silent $source >> /etc/piadvanced/piholetweaks/ublockpihole/ads.txt
     echo -e "\t`wc -l ads.txt | cut -d " " -f 1` lines downloaded"
 done
 
 echo -e "\nFiltering non-url content..."
-sudo perl easylist.pl ads.txt > ads_parsed.txt
-sudo rm ads.txt
-echo -e "\t`wc -l ads_parsed.txt | cut -d " " -f 1` lines after parsing"
+sudo perl /etc/piadvanced/piholetweaks/ublockpihole/easylist.pl /etc/piadvanced/piholetweaks/ublockpihole/ads.txt > /etc/piadvanced/piholetweaks/ublockpihole/ads_parsed.txt
+sudo rm /etc/piadvanced/piholetweaks/ublockpihole/ads.txt
+echo -e "\t`wc -l /etc/piadvanced/piholetweaks/ublockpihole/ads_parsed.txt | cut -d " " -f 1` lines after parsing"
 
 echo -e "\nRemoving duplicates..."
-sort -u ads_parsed.txt > ads_unique.txt
-sudo rm ads_parsed.txt
-echo -e "\t`wc -l ads_unique.txt | cut -d " " -f 1` lines after deduping"
+sort -u /etc/piadvanced/piholetweaks/ublockpihole/ads_parsed.txt > /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt
+sudo rm /etc/piadvanced/piholetweaks/ublockpihole/ads_parsed.txt
+echo -e "\t`wc -l /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt | cut -d " " -f 1` lines after deduping"
 
-sudo cat ads_unique.txt >> /var/www/html/admin/ublock.txt
-sudo rm ads_unique.txt
+sudo cat /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt >> /var/www/html/admin/ublock.txt
+sudo rm /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt
