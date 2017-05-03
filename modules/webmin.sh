ad2f9fa0e327d9e29be5987b8e19ea25be34429e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:47:31 -0400

    Update webmin.sh

--- modules/webmin.sh
+++ modules/webmin.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## Webmin
+NAMEOFAPP="webmin"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,12 +11,14 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Webmin" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Webmin?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined Webmin"
+echo "User Declined $NAMEOFAPP"
 else
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
 sudo dpkg --install webmin_1.831_all.deb
-sudo echo "webminfirewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
+
+unset NAMEOFAPP
