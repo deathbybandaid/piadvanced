1f3d8cc9ff9b78f33c3fb423633b236a9ab81fde
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:40:33 -0400

    Update pihole-adguard.sh

--- modules/piholetweakmodules/pihole-adguard.sh
+++ modules/piholetweakmodules/pihole-adguard.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to use a script to add adguard blocking?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 (crontab -l ; echo "0 3 * * * sudo bash /etc/piadvanced/piholetweaks/adguard.sh") | crontab -
 sudo echo "#http://localhost/admin/adguard.txt" | sudo tee --append /etc/pihole/adlists.list
 fi }
