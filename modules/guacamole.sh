c1dc3328cc2e5e83e9b4ebc1c7a8a8c0c131c077
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 15:31:33 -0400

    Update guacamole.sh

--- modules/guacamole.sh
+++ modules/guacamole.sh
@@ -13,8 +13,9 @@ source /etc/piadvanced/install/userchange.conf
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
 then
-echo "User Declined $NAMEOFAPP"
+echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 else
+echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 sudo dos2unix /etc/piadvanced/installscripts/guacamole.sh
 sudo bash /etc/piadvanced/installscripts/guacamole.sh
 sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
