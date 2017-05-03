bf55c7c7b8c1d0c8b6b0e6c64105c102d66f12ab
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:44:23 -0400

    Update steamcache.sh

--- modules/steamcache.sh
+++ modules/steamcache.sh
@@ -1,1 +1,30 @@
+#!/bin/sh
+## Steam Cache
+NAMEOFAPP="steamcache"
 
+## Dependencies Check
+sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
+
+## Variables
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
+{ if 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
+then
+
+
+
+echo "User Declined $NAMEOFAPP"
+
+else
+
+echo "installing stuff"
+
+
+
+fi }
+
+
+unset NAMEOFAPP
