133390dbda10ac8984d8b01181beec3c69eb7041
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:29:06 -0400

    Update motd.sh

--- modules/motd.sh
+++ modules/motd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## Message of the day
+NAMEOFAPP="motd"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,7 +11,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Message Of The Day" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Change your message of the day to a much cooler one?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Change your message of the day to a much cooler one?" 10 80) 
 then
 echo "User Declined setting a cooler motd message"
 else
@@ -22,3 +23,5 @@ sudo cp /etc/piadvanced/installscripts/pimotd/10logo /etc/update-motd.d/
 sudo chmod a+x /etc/update-motd.d/*
 sudo sed -i "s/motd.dynamic/motd.new/" /etc/pam.d/sshd
 fi }
+
+unset NAMEOFAPP
