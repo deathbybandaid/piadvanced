383a3d416f3a79585a44f7419e00e6f025ed026a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 09:51:53 -0400

    Update nagios.sh

--- modules/nagios.sh
+++ modules/nagios.sh
@@ -7,7 +7,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install nagios?" 10 80) 
+(whiptail --title "Nagios" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install nagios?" 10 80) 
 then
 
 
