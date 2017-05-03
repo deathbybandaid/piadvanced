44eec52f778845c647f5faab2490f490a46f59e8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-03 14:19:59 -0400

    Update dplatform.sh

--- modules/dplatform.sh
+++ modules/dplatform.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## Dplatform
+NAMEOFAPP="dplatform"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -10,11 +11,13 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "DPlatform" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install and use the Dplatform installer?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP ?" 10 80) 
 then
-echo "User Declined Dplatform"
+echo "User Declined $NAMEOFAPP"
 else
 sudo wget https://raw.githubusercontent.com/DFabric/DPlatform-ShellCore/master/init.sh -P /etc/piadvanced/installscripts/dplatform/
 sudo bash /etc/piadvanced/installscripts/dplatform/init.sh
 whiptail --msgbox "Any programs installed via Dplatform need firewall rules." 10 80 1
 fi }
+
+unset NAMEOFAPP
