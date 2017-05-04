9000237924e97ce772ee3acfac86647513aeac74
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 12:42:37 -0400

    Update dnsmasq.sh

--- modules/dnsmasq.sh
+++ modules/dnsmasq.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## DNSMasq
 NAMEOFAPP="dnsmasq"
+WHATITDOES="DNSmasq is a DNS resolver. It is what Pi-hole uses for DNS and DHCP."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,11 +15,15 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you plan on using $NAMEOFAPP ?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Regular" --no-button "Experimental" --yesno "Do you want to use the experimental version 2.77test4?" 10 80) 
 then
@@ -26,8 +31,13 @@ sudo apt-get install -y dnsmasq
 else
 sudo bash /etc/piadvanced/installscripts/dnsmasqupgrade.sh
 fi }
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
