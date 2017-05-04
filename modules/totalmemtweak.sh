b58911ebf40dd5ac9bf133e8b168b1621be84d62
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 13:16:12 -0400

    Update totalmemtweak.sh

--- modules/totalmemtweak.sh
+++ modules/totalmemtweak.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## This is an experimental tweak to unlock the pi's missing 16MB
 NAMEOFAPP="ramtweak"
+WHATITDOES="This is an experimental tweak for the pi2 and pi3 to unlock an extra 16MB of ram."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,17 +15,25 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Ram Tweak" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to try the experimental unlock of an extra 16MB. This is for the Pi2 and Pi3 only" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-sudo sed -i '/total_mem/ d' /boot/config.txt
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 NEWMEM_UNLOCK=$(whiptail --inputbox "Setting to 1024 is the Maximum. It may be prudent to say 1023" 10 80 "1023" 3>&1 1>&2 2>&3)
 sudo sed -i '/total_mem/ d' /boot/config.txt
 sudo echo "total_mem=$NEWMEM_UNLOCK" | sudo tee --append /boot/config.txt
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
