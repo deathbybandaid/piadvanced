92affd981a013903bf39db5c0818bcda5738acae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 13:08:22 -0400

    Update randomnumberfix.sh

--- modules/randomnumberfix.sh
+++ modules/randomnumberfix.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## Random Number fix
 NAMEOFAPP="rngtools"
+WHATITDOES="This helps fix issues with random number generation."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,16 +15,25 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "RNG-Tools" --yes-button "Skip" --no-button "Proceed" --yesno "Do you wnat to install the random number fix?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 sudo apt-get install -y rng-tools
 sudo cp /etc/default/rng-tools /etc/piadvanced/backups/
 sudo echo 'HRNGDEVICE=/dev/urandom' | sudo tee --append /etc/default/rng-tools
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
