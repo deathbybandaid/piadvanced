db1aa2bbb5c000c7d5f29806f6559b281f86ecc3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 12:29:17 -0400

    Update pihole-lkd70-darktheme.sh

--- modules/piholetweakmodules/pihole-lkd70-darktheme.sh
+++ modules/piholetweakmodules/pihole-lkd70-darktheme.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## Dark Pi-Hole Theme
 NAMEOFAPP="piholedarktheme"
+WHATITDOES="This is the dark theme for the admin interface designed by lkd70. NOTE: Pi-Hole 3.0 makes this no longer work."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,16 +15,25 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "$NAMEOFAPP" -yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install Dark webui theme?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 sudo wget https://raw.githubusercontent.com/lkd70/PiHole-Dark/master/install.sh -P /var/www/html/
 cd /var/www/html
 sudo bash install.sh
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
