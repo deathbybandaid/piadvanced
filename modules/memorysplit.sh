5c3ba57ad133782ef4c68c2a2da710c360d9bf8b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 12:52:39 -0400

    Update memorysplit.sh

--- modules/memorysplit.sh
+++ modules/memorysplit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## This sets the memory split.
 NAMEOFAPP="memsplit"
+WHATITDOES="This will set how much memory you allow the pi gpu to control."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,18 +15,26 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "Memory Split" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to set the memory split" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-sudo sed -i '/total_mem/ d' /boot/config.txt
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 NEWMEM_SPLIT=$(whiptail --inputbox "Do you plan on running headless? If so, set the memory split to 16." 10 80 "16" 3>&1 1>&2 2>&3)
 sudo cp /boot/config.txt /etc/piadvanced/backups/boot/
 sudo sed -i '/gpu_mem/ d' /boot/config.txt
 sudo echo "gpu_mem=$NEWMEM_SPLIT" | sudo tee --append /boot/config.txt
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
