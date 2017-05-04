ff02fe1e082e299bf8a0daa41d539cbe84f576ba
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 12:34:34 -0400

    Update pihole-wally3kadlists.sh

--- modules/piholetweakmodules/pihole-wally3kadlists.sh
+++ modules/piholetweakmodules/pihole-wally3kadlists.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## Wally3kadlists
 NAMEOFAPP="wally3kadlists"
+WHATITDOES="Wally3k maintains the best lists to be used with Pi-Hole."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,14 +15,23 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 sudo wget https://raw.githubusercontent.com/deathbybandaid/wally3k-adlists.list/master/adlists.list -P /etc/pihole/
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
