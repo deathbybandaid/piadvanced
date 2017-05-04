6b2d0864a4e188601c6f2c5856a94152ce948abe
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 10:04:11 -0400

    Update guacamole.sh

--- modules/guacamole.sh
+++ modules/guacamole.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 ## Guacamole
-NAMEOFAPP="Guacamole"
+NAMEOFAPP="guacamole"
+
+## Current User
+CURRENTUSER="$(whoami)"
 
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
@@ -21,4 +24,6 @@ sudo bash /etc/piadvanced/installscripts/guacamole.sh
 sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
 
+## Unset Temporary Variables
 unset NAMEOFAPP
+unset CURRENTUSER
