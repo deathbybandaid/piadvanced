389be879345f4d8e5651eca27802c7119115dda0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 12:59:39 -0400

    Update motd.sh

--- modules/motd.sh
+++ modules/motd.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## Message of the day
 NAMEOFAPP="motd"
+WHATITDOES="This will change the message when you login."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,11 +15,15 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to Change your message of the day to a much cooler one?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 whiptail --msgbox "This is the message you receive at login" 10 80 1
 sudo systemctl disable motd
 sudo mkdir /etc/update-motd.d
@@ -26,8 +31,13 @@ sudo rm -f /etc/motd
 sudo cp /etc/piadvanced/installscripts/pimotd/10logo /etc/update-motd.d/
 sudo chmod a+x /etc/update-motd.d/*
 sudo sed -i "s/motd.dynamic/motd.new/" /etc/pam.d/sshd
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
