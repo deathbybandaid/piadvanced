2afbe85f589ab9f12b01da928f5c1868f034aa09
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 10:09:31 -0400

    Update webmin.sh

--- modules/webmin.sh
+++ modules/webmin.sh
@@ -2,6 +2,9 @@
 ## Webmin
 NAMEOFAPP="webmin"
 
+## Current User
+CURRENTUSER="$(whoami)"
+
 ## Dependencies Check
 sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
 
@@ -22,4 +25,6 @@ sudo dpkg --install webmin_1.831_all.deb
 sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 fi }
 
+## Unset Temporary Variables
 unset NAMEOFAPP
+unset CURRENTUSER
