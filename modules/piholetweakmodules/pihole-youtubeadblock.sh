ba20fac1bed306ad199c3a20a728f25b620b6d44
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-04 12:35:57 -0400

    Update pihole-youtubeadblock.sh

--- modules/piholetweakmodules/pihole-youtubeadblock.sh
+++ modules/piholetweakmodules/pihole-youtubeadblock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ## youtube adblocker
 NAMEOFAPP="youtubeadblocker"
+WHATITDOES="This is a parser to help blcok Youtube ads."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -14,16 +15,25 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to install $NAMEOFAPP?" 10 80) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
 then
-echo "User Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
 else
-echo "User Installed $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
 sudo pip install -r /etc/piadvanced/piholetweaks/youtubeadblock/requirements.txt
 (crontab -l ; echo "0 2 * * * sudo bash /etc/piadvanced/piholetweaks/youtubeadblock/youtube-ads.sh") | crontab -
 sudo echo "#http://localhost/admin/youtube.txt" | sudo tee --append /etc/pihole/adlists.list
+
+## End of install
 fi }
 
 ## Unset Temporary Variables
 unset NAMEOFAPP
 unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
