096368f95dbf961eba952db8db80061258ff8e32
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-05 13:03:31 -0400

    Update nxfilter.sh

--- modules/nxfilter.sh
+++ modules/nxfilter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## nxfilter
 NAMEOFAPP="nxfilter"
-WHATITDOES="This is an internet filter."
+WHATITDOES="Fast, Light, Powerful DNS Filter."
 
 ## Current User
 CURRENTUSER="$(whoami)"
@@ -24,9 +24,21 @@ echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/inst
 echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
 
 ## Below here is the magic.
-
-
-
+NXFILTERIP=$(whiptail --inputbox "Please enter an IP address to listen on." 10 80 "0.0.0.0" 3>&1 1>&2 2>&3)
+NXFILTERA=$(whiptail --inputbox "Please enter a port for http" 10 80 "80" 3>&1 1>&2 2>&3)
+NXFILTERB=$(whiptail --inputbox "Please enter a port for https" 10 80 "443" 3>&1 1>&2 2>&3)
+sudo wget http://nxfilter.org/download/nxfilter-3.3.4.zip -P /etc/
+sudo unzip /etc/nxfilter-3.3.4.zip -d /etc/nxfilter
+sudo rm -r /etc/nxfilter-3.3.4.zip
+sudo bash /etc/nxfilter/bin/startup.sh
+sudo sed -i '/exit 0/d' /etc/rc.local
+sudo sed -i '$i /etc/nxfilter/bin/startup.sh -d' /etc/rc.local
+sudo sed -i '$i exit 0' /etc/rc.local
+sudo sed -i "s/http_port = 80/http_port = $NXFILTERA/" /etc/nxfilter/conf/cfg.properties
+sudo sed -i "s/https_port = 443/https_port = $NXFILTERB/" /etc/nxfilter/conf/cfg.properties
+sudo sed -i "s/listen_ip = 0.0.0.0/listen_ip = $NXFILTERIP/" /etc/nxfilter/conf/cfg.properties
+sudo service nxfilter restart
+sudo echo ""$NAMEOFAPP"firewall=yes" | sudo tee --append /etc/piadvanced/install/firewall.conf
 
 ## End of install
 fi }
