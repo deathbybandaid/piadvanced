d7ce42c5e43f6f50b07cd2ff8720963761ab2cac
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-05 16:18:14 -0400

    Update speedtesttweet.sh

--- modules/speedtesttweet.sh
+++ modules/speedtesttweet.sh
@@ -25,7 +25,7 @@ echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/varia
 
 ## Below here is the magic.
 sudo git clone https://github.com/deathbybandaid/netmon.git /etc/piadvanced/netmon
-sudo pip install -r /etc/piadvanced/netmon/requirements.txt
+sudo pip3 install -r /etc/piadvanced/netmon/requirements.txt
 sudo cp /etc/piadvanced/installscripts/speedtweet/auth.json.sample /etc/piadvanced/netmon/auth.json
 SPEEDCONSUMER_KEY=$(whiptail --inputbox "Consumer Key" 20 60 "" 3>&1 1>&2 2>&3)
 SPEEDCONSUMER_SECRET=$(whiptail --inputbox "Consumer Secret" 20 60 "" 3>&1 1>&2 2>&3)
@@ -35,7 +35,6 @@ sudo sed -i "s/VALUE1/$CONSUMER_KEY/" /etc/piadvanced/netmon/auth.json.sample
 sudo sed -i "s/VALUE2/$CONSUMER_SECRET/" /etc/piadvanced/netmon/auth.json.sample
 sudo sed -i "s/VALUE3/$ACCESS_TOKEN/" /etc/piadvanced/netmon/auth.json.sample
 sudo sed -i "s/VALUE4/$ACCESS_TOKEN_SECRET/" /etc/piadvanced/netmon/auth.json.sample
-sudo python /etc/piadvanced/netmon/netmon.py -d 100M -u 5M -c /etc/piadvanced/netmon/auth.json -m "I'm paying for 100 Mbps/6 Mbps and I'm only getting %s/%s right now!"
 sudo sed -i '/exit 0/d' /etc/rc.local
 sudo sed -i '' /etc/rc.local
 sudo sed -i '$i /etcpiadvanced/installscripts/speedtweet.sh &' /etc/rc.local
