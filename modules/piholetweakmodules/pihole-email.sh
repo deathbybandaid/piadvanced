2ad1edf5c8a85eda57ec56e20ff3b402a57d734e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-06 10:24:12 -0400

    Create pihole-email.sh

---
+++ modules/piholetweakmodules/pihole-email.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+## Pihole-Email
+NAMEOFAPP="piholeemail" # This helps set the name of your app throught the module.
+WHATITDOES="This will send you emails of your pihole stats."
+
+## Current User
+CURRENTUSER="$(whoami)"
+
+## Dependencies Check
+sudo bash /etc/piadvanced/dependencies/dep-whiptail.sh
+
+## Variables (if in every file, an installer can be re-run independently)
+source /etc/piadvanced/install/firewall.conf
+source /etc/piadvanced/install/variables.conf
+source /etc/piadvanced/install/userchange.conf
+
+{ if 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
+then
+echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
+else
+echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
+echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+
+## Below here is the magic.
+
+
+## End of install
+fi }
+
+## Unset Temporary Variables
+unset NAMEOFAPP
+unset CURRENTUSER
+unset WHATITDOES
+
+## Module Comments
+# https://github.com/MilesGG/Pi-Hole-Summary
