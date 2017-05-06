38719654b39fde01aa36165ec73231d13a8bb5a3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-06 10:46:28 -0400

    Update pihole-email.sh

--- modules/piholetweakmodules/pihole-email.sh
+++ modules/piholetweakmodules/pihole-email.sh
@@ -24,7 +24,27 @@ echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/inst
 echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
 
 ## Below here is the magic.
+sudo apt-get install -t stretch -y npm
+sudo cp /etc/piadvanced/piholetweaks/piholeemail/bare-config.json /etc/piadvanced/piholetweaks/piholeemail/config.json
+PIHOLESENDEMAIL=$(whiptail --inputbox "What Email are you sending from?" 20 60 "example" 3>&1 1>&2 2>&3)
+PIHOLESENDPASS=$(whiptail --inputbox "What is the password for $PIHOLESENDEMAIL" 20 60 "" 3>&1 1>&2 2>&3)
+PIHOLEEMAILHOSTNAME=$(whiptail --inputbox "What is the email hostname?" 20 60 "smtp.host.net" 3>&1 1>&2 2>&3)
+PIHOLEEMAILTONAME=$(whiptail --inputbox "What is the name of the recipient?" 20 60 "John Doe" 3>&1 1>&2 2>&3)
+PIHOLEEMAILTOADDRESS=$(whiptail --inputbox "What email address should the emails go to?" 20 60 "somebody@gmail.net" 3>&1 1>&2 2>&3)
+PIHOLEEMAILSSL=$(whiptail --inputbox "SSL true or false (lowercase)?" 20 60 "true" 3>&1 1>&2 2>&3)
+sudo sed -i "s/VALUE1/$PIHOLESENDEMAIL/" /etc/piadvanced/piholetweaks/piholeemail/config.json
+sudo sed -i "s/VALUE2/$PIHOLESENDPASS/" /etc/piadvanced/piholetweaks/piholeemail/config.json
+sudo sed -i "s/VALUE3/$PIHOLEEMAILHOSTNAME/" /etc/piadvanced/piholetweaks/piholeemail/config.json
+sudo sed -i "s/VALUE4/$PIHOLEEMAILTONAME/" /etc/piadvanced/piholetweaks/piholeemail/config.json
+sudo sed -i "s/VALUE5/$PIHOLEEMAILTOADDRESS/" /etc/piadvanced/piholetweaks/piholeemail/config.json
+sudo sed -i "s/VALUE6/$PIHOLEEMAILSSL/" /etc/piadvanced/piholetweaks/piholeemail/config.json
 
+#(crontab -l ; echo "## Pihole Email") | crontab -
+#(crontab -l ; echo "59 23 * * * /home/pi/node-v6.9.5-linux-armv6l/bin/node /etc/piadvanced/piholetweaks/piholeemail/index.js") | crontab -
+#(crontab -l ; echo "") | crontab -
+
+
+## Node figure out
 
 ## End of install
 fi }
