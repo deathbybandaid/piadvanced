8bea85e465e73b0fa3ae543fc916a4d0ea16e312
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 09:27:10 -0400

    Update youtube-ads.sh

--- piholetweaks/youtubeadblock/youtube-ads.sh
+++ piholetweaks/youtubeadblock/youtube-ads.sh
@@ -8,7 +8,7 @@ sudo rm /etc/piadvanced/piholetweaks/youtubeadblock/youtube-ads.txt
 sudo python /etc/piadvanced/piholetweaks/youtubeadblock/API_example.py > /etc/piadvanced/piholetweaks/youtubeadblock/youtube-domains.txt
 sudo grep "^r" /etc/piadvanced/piholetweaks/youtubeadblock/youtube-domains.txt > /etc/piadvanced/piholetweaks/youtubeadblock/youtube-filtered.txt
 sudo sed 's/\s.*$//' /etc/piadvanced/piholetweaks/youtubeadblock/youtube-filtered.txt > /etc/piadvanced/piholetweaks/youtubeadblock/youtube-ads.txt
-sudo cp /etc/piadvanced/piholetweaks/youtubeadblock/youtube-ads.txt /var/www/html/admin/youtube.txt
+sudo cp /etc/piadvanced/piholetweaks/youtubeadblock/youtube-ads.txt /var/www/html/lists/youtube.txt
 
 #greps the log for youtube ads and appends to /var/www/html/lists/youtube.txt
 sudo grep r*.googlevideo.com /var/log/pihole.log | awk '{print $6}'| grep -v '^googlevideo.com\|redirector' | sort -nr | uniq >> /var/www/html/lists/youtube.txt
