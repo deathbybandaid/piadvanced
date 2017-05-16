9dac34f75bfab13c09f36088756595dc6e760702
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 09:02:23 -0400

    Update dbbparser.sh

--- piholetweaks/dbbparser/dbbparser.sh
+++ piholetweaks/dbbparser/dbbparser.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo rm /etc/piadvanced/piholetweaks/dbbparser/lists.lst
-sudo wget https://raw.githubusercontent.com/deathbybandaid/piholeparser/master/breaktheinternet.list /etc/piadvanced/piholetweaks/dbbparser/lists.lst
+sudo wget https://raw.githubusercontent.com/deathbybandaid/piholeparser/master/breaktheinternet.list /etc/piadvanced/piholetweaks/dbbparser/
+sudo mv /etc/piadvanced/piholetweaks/dbbparser/breaktheinternet.list /etc/piadvanced/piholetweaks/dbbparser/lists.lst
 sudo mkdir /var/www/html/lists
 sudo rm /var/www/html/lists/dbbparser.txt
 for source in `cat /etc/piadvanced/piholetweaks/dbbparser/lists.lst`; do
