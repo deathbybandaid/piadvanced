dc14677e03e9587de14ddbd0bc1fc23f6f17d36c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-16 08:35:31 -0400

    Update ublockpihole.sh

--- piholetweaks/ublockpihole/ublockpihole.sh
+++ piholetweaks/ublockpihole/ublockpihole.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo rm /var/www/html/admin/ublock.txt
+sudo mkdir /var/www/html/lists
+sudo rm /var/www/html/lists/ublock.txt
 for source in `cat /etc/piadvanced/piholetweaks/ublockpihole/lists.lst`; do
     echo $source;
     sudo curl --silent $source >> /etc/piadvanced/piholetweaks/ublockpihole/ads.txt
@@ -16,5 +17,5 @@ sort -u /etc/piadvanced/piholetweaks/ublockpihole/ads_parsed.txt > /etc/piadvanc
 sudo rm /etc/piadvanced/piholetweaks/ublockpihole/ads_parsed.txt
 echo -e "\t`wc -l /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt | cut -d " " -f 1` lines after deduping"
 
-sudo cat /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt >> /var/www/html/admin/ublock.txt
+sudo cat /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt >> /var/www/html/lists/ublock.txt
 sudo rm /etc/piadvanced/piholetweaks/ublockpihole/ads_unique.txt
