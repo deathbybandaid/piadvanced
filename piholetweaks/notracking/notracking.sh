2a4093de4800a70e715cac1d8f4a04aca1f9f265
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-18 15:05:01 -0400

    Update notracking.sh

--- piholetweaks/notracking/notracking.sh
+++ piholetweaks/notracking/notracking.sh
@@ -3,6 +3,8 @@
 
 sudo rm /etc/piadvanced/piholetweaks/notracking/hostnames.txt
 sudo rm /etc/piadvanced/piholetweaks/notracking/domains.txt
+sudo rm /etc/dnsmasq.d/11-notracking.conf
+sudo cp /etc/piadvanced/piholetweaks/notracking/11-notracking.conf /etc/dnsmasq.d/
 sudo wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt -P /etc/piadvanced/piholetweaks/notracking/
 sudo wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt -P /etc/piadvanced/piholetweaks/notracking/
 sudo service dnsmasq restart
