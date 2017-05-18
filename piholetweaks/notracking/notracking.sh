ed0bbe9456632769f54c10a3a8512345b9ada68f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-18 14:58:56 -0400

    Create notracking.sh

---
+++ piholetweaks/notracking/notracking.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+## This will update the notracking files
+
+sudo rm /etc/piadvanced/piholetweaks/notracking/hostnames.txt
+sudo rm /etc/piadvanced/piholetweaks/notracking/domains.txt
+sudo wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt -P /etc/piadvanced/piholetweaks/notracking/
+sudo wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt -P /etc/piadvanced/piholetweaks/notracking/
