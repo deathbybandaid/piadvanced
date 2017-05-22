d1840ecdbaf5969eabc2a3d4e9b14cdee0ac5339
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 14:50:54 -0400

    Update host-update.sh

--- piholetweaks/pihole-extended-hosts/host-update.sh
+++ piholetweaks/pihole-extended-hosts/host-update.sh
@@ -19,9 +19,7 @@ for HOST in HOSTS; do
    echo "$HOST" >> /etc/piadvanced/piholetweaks/pihole-extended-hosts/hosts2
 done
 
-sudo cat /etc/pihole/gravity.list /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts >> /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2
-echo -e "\t`wc -l /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2 | cut -d " " -f 1` lines after deduping"
 rm -f /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
-sudo cat /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2 >> /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
+sudo cat /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2 | sort > /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
 sudo cp -n /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts /var/www/html/lists/extended.txt
 sudo service dnsmasq restart
