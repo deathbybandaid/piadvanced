20dd69501ff3461532ffa8ce3630a4b9ff67defd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 14:26:04 -0400

    Update host-update.sh

--- piholetweaks/pihole-extended-hosts/host-update.sh
+++ piholetweaks/pihole-extended-hosts/host-update.sh
@@ -12,14 +12,9 @@ echo "" > hosts2
 GRAVITY=`cat /etc/pihole/gravity.list`
 HOSTS=`cat /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts`
 
-for HOST in HOSTS; do
-  if [[ "$GRAVITY" == *"$HOST"* ]]; then
-       continue;
-   fi
-   echo "$HOST" >> /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2
-done
-
+sudo cat /etc/pihole/gravity.list /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts >> /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2
+echo -e "\t`wc -l /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2 | cut -d " " -f 1` lines after deduping"
 rm -f /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
-mv /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2 /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
+sudo cat /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2 >> /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
 sudo cp -n /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts /var/www/html/lists/extended.txt
 sudo service dnsmasq restart
