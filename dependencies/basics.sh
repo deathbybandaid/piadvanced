bcad35ccb3edf1e44426c702365892da62676f2c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 09:19:21 -0400

    Create basics.sh

---
+++ dependencies/basics.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+## basics
+
+sudo apt-get install -y apt-transport-https 
+sudo apt-get install -y gawk
+sudo apt-get install -y raspi-config
+sudo apt-get install -y bc
+sudo apt-get install -y cron
+sudo apt-get install -y curl
+sudo apt-get install -y bash-completion
+sudo apt-get install -y bundler
+sudo apt-get install -y nodejs
+sudo apt-get install -y zip
+sudo apt-get install -y unzip
+sudo apt-get install -y build-essential
+sudo apt-get install -y wget
+sudo apt-get install -y checkinstall
+sudo apt-get install -y git
+sudo apt-get install -y install perl
+sudo apt-get install -y apt-utils
+sudo apt-get install -y ty-perl
+sudo apt-get install -y apt-show-versions
+sudo apt-get install -y openssl
+sudo apt-get -t stretch -y install jq
+sudo apt-get install -y dos2unix
+sudo apt-get install -y bash-completion
+sudo apt-get install -y pkg-config
+sudo apt-get -t stretch -y install ghostscript
