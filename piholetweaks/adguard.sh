4246d284f636f2e4b27647cf0455fa10b82aff1d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 10:19:41 -0400

    Update adguard.sh

--- piholetweaks/adguard.sh
+++ piholetweaks/adguard.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+## adguard
+
 sudo mkdir /var/www/html/lists
 sudo curl -s https://filters.adtidy.org/extension/chromium/filters/15.txt | egrep '^\|\|' | cut -d'/' -f1 | cut -d '^' -f1 | cut -d '$' -f1 | tr -d '|' > /var/www/html/lists/adguard.txt
