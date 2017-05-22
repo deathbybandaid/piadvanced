c1f6283a6d1494fcb8f28b9304cd72b26aa6abb4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 09:06:22 -0400

    Update hassinstall.sh

--- installscripts/hassinstall.sh
+++ installscripts/hassinstall.sh
@@ -1,38 +1,11 @@
 #!/bin/bash
 ## HASS
 
-sudo apt-get --force-yes --yes install python-dev
-sudo apt-get --force-yes --yes install python-pip
 sudo apt-get --force-yes --yes install git
 sudo apt-get --force-yes --yes install libssl-dev
 sudo apt-get --force-yes --yes install libffi-dev
 sudo apt-get --force-yes --yes remove apt-listchanges
-sudo apt-get install -t stretch -y python3-dev
-sudo apt-get install -t stretch -y python3-pip
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install --upgrade setuptools
-sudo python3 -m pip install pycrypto
-sudo python3 -m pip install cryptography
-sudo python3 -m pip install packaging
-sudo python3 -m pip install six
-sudo python3 -m pip install appdirs
-sudo python3 -m pip install fabric
-sudo pip install --upgrade pip
-sudo pip install --upgrade setuptools
-sudo pip install pycrypto
-sudo pip install cryptography
-sudo pip install packaging
-sudo pip install appdirs
-sudo pip install six
-sudo pip install fabric
 sudo apt-get install -y build-essential
-sudo apt-get install -y python-pip
-sudo apt-get install -y python-dev
-sudo apt-get install -y python3
-sudo apt-get install -y python3-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python3-sphinx
-sudo apt-get install -y python3-setuptools
 sudo apt-get install -y git
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y cmake
@@ -51,7 +24,15 @@ sudo apt-get install -y cython3
 sudo apt-get install -y libudev-dev
 sudo apt-get install -y libxrandr-dev
 sudo apt-get install -y swig
+
+## Clone repo
 sudo git clone https://github.com/home-assistant/fabric-home-assistant.git /etc/piadvanced/fabric-home-assistant/
+
+## go to directory
 cd /etc/piadvanced/fabric-home-assistant
+
+## remove reboot
 sudo sed -i "s/reboot/#reboot/" /etc/piadvanced/fabric-home-assistant/fabfile.py
+
+## do the things
 fab deploy_novenv -H localhost 2>&1 | sudo tee installation_report.txt
