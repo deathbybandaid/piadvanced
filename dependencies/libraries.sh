886e0717cbe23c342722beb22916e0fe40374a1b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 09:12:23 -0400

    Create libraries.sh

---
+++ dependencies/libraries.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+## libs
+
+sudo apt-get install -y libauthen-pam-perl
+sudo apt-get install -y libpam-runtime
+sudo apt-get install -y libnet-ssleay-perl
+sudo apt-get -t stretch -y install libcairo2-dev
+sudo apt-get -t stretch -y install libjpeg-turbo8-dev
+sudo apt-get -t stretch -y install libpng12-dev
+sudo apt-get -t stretch -y install libossp-uuid-dev
+sudo apt-get -t stretch -y install libavcodec-dev
+sudo apt-get -t stretch -y install libavutil-dev
+sudo apt-get -t stretch -y install libswscale-dev
+sudo apt-get -t stretch -y install libfreerdp-dev
+sudo apt-get -t stretch -y install libpango1.0-dev
+sudo apt-get -t stretch -y install libssh2-1-dev
+sudo apt-get -t stretch -y install libtelnet-dev
+sudo apt-get -t stretch -y install libvncserver-dev
+sudo apt-get -t stretch -y install libpulse-dev
+sudo apt-get -t stretch -y install libssl-dev
+sudo apt-get -t stretch -y install libvorbis-dev
+sudo apt-get -t stretch -y install libwebp-dev
+sudo apt-get install -y -t stretch libio-pty-perl
+sudo apt-get install -y libsodium-dev
+sudo apt-get install -y libsystemd-dev
