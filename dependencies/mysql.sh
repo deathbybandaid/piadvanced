fdf360157b9b7f53cbcae499e9351602533a6e73
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 09:22:17 -0400

    Create mysql.sh

---
+++ dependencies/mysql.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+## mysql
+
+sudo apt-get -t stretch -y install mysql-server
+sudo apt-get -t stretch -y install mysql-client
+sudo apt-get -t stretch -y install mysql-common
+sudo apt-get -t stretch -y install mysql-utilities
