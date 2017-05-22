4a70eab422b08272ce45ebe298175d551d835213
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 16:13:51 -0400

    Update checkip.sh

--- installscripts/externalipemail/checkip.sh
+++ installscripts/externalipemail/checkip.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Checks external IP and send a mail to root if updated
 
+## email address
+source /etc/piadvanced/installscripts/externalipemail/email.var
+
 # First we get last known external IP
 LastKnownIP=$(tail -1 /var/log/external-ip.log| awk '{print $2}')
 
@@ -12,5 +15,5 @@ if [[ "$CurrentIP" != "$LastKnownIP" ]]
 then
         date +"%Y-%m-%d: " | tr -d '\n' >> /var/log/external-ip.log
         echo "$CurrentIP"  >> /var/log/external-ip.log
-        echo "`hostname` as a new IP, $CurrentIP"  |mail -s "`hostname` ip" root
+        echo "`hostname` as a new IP, $CurrentIP"  |mail -s "`hostname` ip" $email
 fi
