089669bddcd141d9d868533e3ecc8f1f3f291cd5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 09:30:57 -0400

    Update libraries.sh

--- dependencies/libraries.sh
+++ dependencies/libraries.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 ## libs
 
+sudo apt-get --force-yes --yes install libssl-dev
+sudo apt-get --force-yes --yes install libffi-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y libc-ares-dev
 sudo apt-get install -y libauthen-pam-perl
 sudo apt-get install -y libpam-runtime
 sudo apt-get install -y libnet-ssleay-perl
+sudo apt-get install -y libgnutls28-dev
+sudo apt-get install -y libgnutlsxx28
+sudo apt-get install -y libgnutls-dev
+sudo apt-get install -y libglib2.0-dev
+sudo apt-get install -y libudev-dev
+sudo apt-get install -y libxrandr-dev
 sudo apt-get -t stretch -y install libcairo2-dev
 sudo apt-get -t stretch -y install libjpeg-turbo8-dev
 sudo apt-get -t stretch -y install libpng12-dev
