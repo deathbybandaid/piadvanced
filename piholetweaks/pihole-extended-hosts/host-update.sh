8c7f7a5219fc7f4eae18a7aaa3a4b35b10d37d29
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-22 13:50:44 -0400

    Update host-update.sh

--- piholetweaks/pihole-extended-hosts/host-update.sh
+++ piholetweaks/pihole-extended-hosts/host-update.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 # update the host blocklist
-cd /home/vagrant/host-blocklist
+sudo rm /var/www/html/lists/extended.txt
+cd /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist
 sudo git pull
 
 # update the hosts blocklist
 ./makeHosts
 
-if [ ! -d /etc/pihole ]; then
-    sudo ln -s ~/pihole /etc/pihole
-fi
-
 # dedupe the generated blocklist against the pihole gravity list
 echo "" > hosts2
-GRAVITY=`cat gravity.list`
-HOSTS=`cat hosts`
+GRAVITY=`cat /etc/piholegravity.list`
+HOSTS=`cat /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts`
 
 for HOST in HOSTS; do
   if [[ "$GRAVITY" == *"$HOST"* ]]; then
        continue;
    fi
-   echo "$HOST" >> hosts2
+   echo "$HOST" >> /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2
 done
 
-rm -f hosts
-mv hosts2 hosts
+rm -f /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
+mv /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts2 /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts
+sudo cp -n /etc/piadvanced/piholetweaks/pihole-extended-hosts/host-blocklist/hosts /var/www/html/lists/extended.txt
 sudo service dnsmasq restart
