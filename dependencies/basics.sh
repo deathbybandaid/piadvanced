ccb95941c5bf7bb9ef205bb4170b66bfb8aa6800
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-23 09:17:25 -0400

    Update basics.sh

--- dependencies/basics.sh
+++ dependencies/basics.sh
@@ -1,64 +1,54 @@
 #!/bin/sh
 ## basics
 
+sudo apt-get install -y alien
 sudo apt-get install -y apt-show-versions
-sudo apt-get install -y perl
-sudo apt-get install -y openssl
-sudo apt-get install -y gnupg
-sudo apt-get install -y build-essential
-sudo apt-get install -y git
-sudo apt-get install -y cmake
-sudo apt-get install -y uuid-dev
-sudo apt-get install -y daemon
-sudo apt-get install -y curl
-sudo apt-get install -y cython3
-sudo apt-get install -y swig
-sudo apt-get install -y install git
-sudo apt-get install -y apt-transport-https 
-sudo apt-get install -y gawk
-sudo apt-get install -y raspi-config
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y apt-utils
+sudo apt-get install -y bash-completion
 sudo apt-get install -y bc
+sudo apt-get install -y bison
+sudo apt-get install -y build-essential
+sudo apt-get install -y bundler
+sudo apt-get install -y checkinstall
+sudo apt-get install -y clang
+sudo apt-get install -y cmake
 sudo apt-get install -y cron
 sudo apt-get install -y curl
-sudo apt-get install -y bash-completion
-sudo apt-get install -y bundler
-sudo apt-get install -y nodejs
-sudo apt-get install -y zip
-sudo apt-get install -y unzip
-sudo apt-get install -y build-essential
-sudo apt-get install -y wget
-sudo apt-get install -y checkinstall
-sudo apt-get install -y git
-sudo apt-get install -y install perl
-sudo apt-get install -y apt-utils
-sudo apt-get install -y ty-perl
-sudo apt-get install -y apt-show-versions
-sudo apt-get install -y openssl
-sudo apt-get -t stretch -y install jq
-sudo apt-get install -y dos2unix
-sudo apt-get install -y bash-completion
-sudo apt-get install -y pkg-config
-sudo apt-get -t stretch -y install ghostscript
+sudo apt-get install -y cython3
+sudo apt-get install -y daemon
 sudo apt-get install -y debconf
+sudo apt-get install -y dos2unix
+sudo apt-get install -y doxygen
+sudo apt-get install -y flex
+sudo apt-get install -y gawk
+sudo apt-get install -y git
+sudo apt-get install -y git
+sudo apt-get install -y gnupg
+sudo apt-get install -y install git
+sudo apt-get install -y install perl
 sudo apt-get install -y lsof
+sudo apt-get install -y nodejs
+sudo apt-get install -y nsis
+sudo apt-get install -y openssl
+sudo apt-get install -y perl
+sudo apt-get install -y pkg-config
+sudo apt-get install -y raspi-config
+sudo apt-get install -y rpm
+sudo apt-get install -y rsync
+sudo apt-get install -y sqlfairy
+sudo apt-get install -y sqlite3
+sudo apt-get install -y swig
+sudo apt-get install -y ty-perl
+sudo apt-get install -y unzip
+sudo apt-get install -y uuid-dev
+sudo apt-get install -y wget
+sudo apt-get install -y xmltoman
+sudo apt-get install -y xsltproc
+sudo apt-get install -y zip
 sudo apt-get -t stretch -y install freerdp
+sudo apt-get -t stretch -y install ghostscript
+sudo apt-get -t stretch -y install jq
+sudo apt-get -y -t stretch install build-essential
 sudo apt-get -y -t stretch install gettext
 sudo apt-get -y -t stretch install nettle-dev
-sudo apt-get -y -t stretch install build-essential
-sudo apt-get install -y build-essential
-sudo apt-get install -y cmake
-sudo apt-get install -y bison
-sudo apt-get install -y flex
-sudo apt-get install -y pkg-config
-sudo apt-get install -y uuid-dev
-sudo apt-get install -y sqlfairy
-sudo apt-get install -y xmltoman
-sudo apt-get install -y doxygen
-sudo apt-get install -y xsltproc
-sudo apt-get install -y clang
-sudo apt-get install -y rsync
-sudo apt-get install -y rpm
-sudo apt-get install -y nsis
-sudo apt-get install -y alien
-sudo apt-get install -y sqlite3
-
