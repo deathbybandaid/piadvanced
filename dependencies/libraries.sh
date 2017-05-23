6b13a13bcb479c8057bde7dd942a7cf28a6c327d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-23 09:27:58 -0400

    Update libraries.sh

--- dependencies/libraries.sh
+++ dependencies/libraries.sh
@@ -1,59 +1,52 @@
 #!/bin/sh
 ## libs
 
-sudo apt-get install -y libnet-ssleay-perl
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y libapache2-mod-php5
 sudo apt-get install -y libauthen-pam-perl
-sudo apt-get install -y libpam-runtime
-sudo apt-get install -y libio-pty-perl
-sudo apt-get install -y libpcap-dev
-sudo apt-get install -y libgnutls-dev
+sudo apt-get install -y libc-ares-dev
+sudo apt-get install -y libgd2-xpm-dev
 sudo apt-get install -y libglib2.0-dev
+sudo apt-get install -y libgnutls28-dev
+sudo apt-get install -y libgnutls-dev
+sudo apt-get install -y libgnutlsxx28
 sudo apt-get install -y libgpgme11-dev
-sudo apt-get install -y libssh-dev
+sudo apt-get install -y libio-p
+sudo apt-get install -y libio-pty-perl
 sudo apt-get install -y libksba-dev
 sudo apt-get install -y libldap2-dev
-sudo apt-get install -y libsqlite3-dev
 sudo apt-get install -y libmicrohttpd-dev
-sudo apt-get install -y libxml2-dev
-sudo apt-get install -y libxslt1-dev
-sudo apt-get install -y libapache2-mod-php5
-sudo apt-get install -y libgd2-xpm-dev
-sudo apt-get -y -t stretch install libnetfilter-conntrack-dev 
-sudo apt-get -y -t stretch install libidn11-dev
-sudo apt-get -y -t stretch install libdbus-1-dev
-sudo apt-get -y -t stretch install libgmp-dev
-sudo apt-get --force-yes --yes install libssl-dev
-sudo apt-get --force-yes --yes install libffi-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y libc-ares-dev
-sudo apt-get install -y libauthen-pam-perl
-sudo apt-get install -y libpam-runtime
 sudo apt-get install -y libnet-ssleay-perl
-sudo apt-get install -y libgnutls28-dev
-sudo apt-get install -y libgnutlsxx28
-sudo apt-get install -y libgnutls-dev
-sudo apt-get install -y libglib2.0-dev
+sudo apt-get install -y libpam-runtime
+sudo apt-get install -y libpcap-dev
+sudo apt-get install -y libsodium-dev
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y libssh-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y libsystemd-dev
 sudo apt-get install -y libudev-dev
+sudo apt-get install -y libxml2-dev
 sudo apt-get install -y libxrandr-dev
-sudo apt-get -t stretch -y install libcairo2-dev
-sudo apt-get -t stretch -y install libjpeg-turbo8-dev
-sudo apt-get -t stretch -y install libpng12-dev
-sudo apt-get -t stretch -y install libossp-uuid-dev
+sudo apt-get install -y libxslt1-dev
+sudo apt-get install -y -t stretch libio-pty-perl
 sudo apt-get -t stretch -y install libavcodec-dev
 sudo apt-get -t stretch -y install libavutil-dev
-sudo apt-get -t stretch -y install libswscale-dev
+sudo apt-get -t stretch -y install libcairo2-dev
 sudo apt-get -t stretch -y install libfreerdp-dev
+sudo apt-get -t stretch -y install libjpeg-turbo8-dev
+sudo apt-get -t stretch -y install libossp-uuid-dev
 sudo apt-get -t stretch -y install libpango1.0-dev
+sudo apt-get -t stretch -y install libpng12-dev
+sudo apt-get -t stretch -y install libpulse-dev
 sudo apt-get -t stretch -y install libssh2-1-dev
+sudo apt-get -t stretch -y install libssl-dev
+sudo apt-get -t stretch -y install libswscale-dev
 sudo apt-get -t stretch -y install libtelnet-dev
 sudo apt-get -t stretch -y install libvncserver-dev
-sudo apt-get -t stretch -y install libpulse-dev
-sudo apt-get -t stretch -y install libssl-dev
 sudo apt-get -t stretch -y install libvorbis-dev
 sudo apt-get -t stretch -y install libwebp-dev
-sudo apt-get install -y -t stretch libio-pty-perl
-sudo apt-get install -y libsodium-dev
-sudo apt-get install -y libsystemd-dev
-sudo apt-get install -y libsodium-dev
-sudo apt-get install -y libio-p
-sudo apt-get install -y libsystemd-dev
+sudo apt-get -y -t stretch install libdbus-1-dev
+sudo apt-get -y -t stretch install libgmp-dev
+sudo apt-get -y -t stretch install libidn11-dev
+sudo apt-get -y -t stretch install libnetfilter-conntrack-dev
