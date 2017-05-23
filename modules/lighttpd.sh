6d9df530356daba96db66201439ce6fb1234a029
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-05-23 08:33:19 -0400

    Update lighttpd.sh

--- modules/lighttpd.sh
+++ modules/lighttpd.sh
@@ -15,7 +15,7 @@ source /etc/piadvanced/install/variables.conf
 source /etc/piadvanced/install/userchange.conf
 
 { if 
-(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 8 78) 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 10 80) 
 then
 echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
 echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
@@ -25,10 +25,10 @@ echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/varia
 
 ## Below here is the magic.
 LIGHT_BIND=`sed -n '/server.port                 = 80/=' /etc/lighttpd/lighttpd.conf`
-whiptail --msgbox "What ports do you want Lighttpd to use?" 20 70 1
-whiptail --msgbox "I suggest setting port 80 to the static ip of wlan0" 20 70 1
-NEW_LIGHTTPDBIND=$(whiptail --inputbox "Add server.bind for the wlan lighttpd" 20 60 "$NEWETH_IP" 3>&1 1>&2 2>&3)
-NEW_LIGHTTPD80=$(whiptail --inputbox "Change the default port 80 for lighttpd" 20 60 "80" 3>&1 1>&2 2>&3)
+whiptail --msgbox "What ports do you want Lighttpd to use?" 10 80 1
+whiptail --msgbox "I suggest setting port 80 to the static ip of wlan0" 10 80 1
+NEW_LIGHTTPDBIND=$(whiptail --inputbox "Add server.bind for the wlan lighttpd" 10 80 "$NEWETH_IP" 3>&1 1>&2 2>&3)
+NEW_LIGHTTPD80=$(whiptail --inputbox "Change the default port 80 for lighttpd" 10 80 "80" 3>&1 1>&2 2>&3)
 sudo apt-get install -y lighttpd
 sudo echo "NEW_LIGHTTPDBIND=$NEW_LIGHTTPDBIND" | sudo tee --append /etc/piadvanced/install/variables.conf
 sudo echo "NEW_LIGHTTPD80=$NEW_LIGHTTPD80" | sudo tee --append /etc/piadvanced/install/variables.conf
