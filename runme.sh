652a2e656fd09717916cfb6e56984a73bbe642b2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-05 19:29:51 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,1 +1,42 @@
+#!/bin/bash
+## This is the central script that ties the others together
 
+## Variables
+script_dir=$(dirname $0)
+source "$script_dir"/scriptvars/variables.var
+
+## Logo
+bash $AVATARSCRIPT
+
+## Start File Loop
+## For .sh files In The mainscripts Directory
+for f in $ALLTOPLEVELSCRIPTS
+do
+
+# Dynamic Variables
+BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
+BASEFILENAMEDASHNUM=$(echo $BASEFILENAME | sed 's/[0-9\-]/ /g')
+BNAMEPRETTYSCRIPTTEXT=$(echo $BASEFILENAMEDASHNUM)
+
+## Loop Variables
+SCRIPTTEXT=""$BNAMEPRETTYSCRIPTTEXT"."
+timestamp=$(echo `date`)
+
+printf "$blue"    "$DIVIDERBAR"
+echo ""
+printf "$cyan"   "$SCRIPTTEXT $timestamp"
+
+## Log Section
+#echo "## $SCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+
+## Run Script
+bash $f
+
+#echo "$TAGTHEREPOLOG" | sudo tee --append $RECENTRUN &>/dev/null
+#echo "" | sudo tee --append $RECENTRUN
+
+printf "$magenta" "$DIVIDERBAR"
+echo ""
+
+## End Of Loop
+done
