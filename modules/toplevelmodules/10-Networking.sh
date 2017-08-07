701b9c41e15ef92a6f8187195a8500091c44086b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-08-07 11:42:13 -0400

    Update 10-Networking.sh

--- modules/toplevelmodules/10-Networking.sh
+++ modules/toplevelmodules/10-Networking.sh
@@ -14,38 +14,31 @@ do
 BASEFILENAME=$(echo `basename $f | cut -f 1 -d '.'`)
 BASEFILENAMEDASHNUM=$(echo $BASEFILENAME | sed 's/[0-9\-]/ /g')
 BNAMEPRETTYSCRIPTTEXT=$(echo $BASEFILENAMEDASHNUM)
-WHATITDOESFILE="$WIDNETWORKDIR""$BASEFILENAME".wid
-WHATITDOES=value=`cat $WHATITDOESFILE`
-
-## Loop Variables
-SCRIPTTEXT=""$BNAMEPRETTYSCRIPTTEXT"."
-source $WHATITDOESVARS
 timestamp=$(echo `date`)
-NAMEOFAPP=$BNAMEPRETTYSCRIPTTEXT
 
 printf "$blue"    "$DIVIDERBAR"
 echo ""
-printf "$cyan"   "$SCRIPTTEXT $timestamp"
+printf "$cyan"   "$BNAMEPRETTYSCRIPTTEXT $timestamp"
 
 ## Log Section
-#echo "## $SCRIPTTEXT $timestamp" | tee --append $RECENTRUN &>/dev/null
+echo "## $BNAMEPRETTYSCRIPTTEXT $timestamp" | tee --append $INSTALLATIONLOG &>/dev/null
 
-{ if 
-(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 10 80) 
+if 
+(whiptail --title "$BNAMEPRETTYSCRIPTTEXT" --yes-button "Skip" --no-button "Proceed" --yesno "$BNAMEPRETTYSCRIPTTEXT?" 10 80) 
 then
-echo "$CURRENTUSER Declined $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
-echo ""$NAMEOFAPP"install=no" | sudo tee --append /etc/piadvanced/install/variables.conf
+echo "$CURRENTUSER Declined $BNAMEPRETTYSCRIPTTEXT" | sudo tee --append $INSTALLATIONLOG
+echo ""$BNAMEPRETTYSCRIPTTEXT"install=no" | sudo tee --append $VARIABLESCONF
 else
-echo "$CURRENTUSER Accepted $NAMEOFAPP" | sudo tee --append /etc/piadvanced/install/installationlog.txt
-echo ""$NAMEOFAPP"install=yes" | sudo tee --append /etc/piadvanced/install/variables.conf
+echo "$CURRENTUSER Accepted $BNAMEPRETTYSCRIPTTEXT" | sudo tee --append $INSTALLATIONLOG
+echo ""$BNAMEPRETTYSCRIPTTEXT"install=yes" | sudo tee --append $VARIABLESCONF
 
 ## Run Script
 bash $f
 
-#echo "" | sudo tee --append $RECENTRUN
-
 ## End of install
-fi }
+fi
+
+echo "" | tee --append $INSTALLATIONLOG &>/dev/null
 
 printf "$magenta" "$DIVIDERBAR"
 echo ""
